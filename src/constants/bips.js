1028a7ae25c58747a87443f14c65ee6d485ec5a3
publius <beanstalk.publius@protonmail.com>
2021-10-13 13:45:32 -0400

    updates

--- src/constants/bips.js
+++ src/constants/bips.js
@@ -1,4 +1,4 @@
 export const bipsList = [
-  {path: '/Bips/BIP-0.html', title: 'Silo Refactor'},
-  {path: '/Bips/BIP-1.html', title: 'Budget Allocation'},
+  {path: '/Bips/bip-0.html', title: 'Silo Refactor'},
+  {path: '/Bips/bip-1.html', title: 'First Budget'},
 ]
