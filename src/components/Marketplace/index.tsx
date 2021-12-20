586b4dd844c039590ef6fa786213a321dcfa46e1
mindsgn <seni.tembe@protonmail.com>
2021-12-20 04:22:19 +0200

    radio button

--- src/components/Marketplace/index.tsx
+++ src/components/Marketplace/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Box, TextField } from '@material-ui/core';
 import Plots from '../Plots';
+import SellPlots from '../SellPlots';
 import Pagination from '../Pagination';
+import DropDown from '../DropDown';
 
 export default function Marketplace() {
     const [plots, setPlots] = React.useState([{ placeInLine: 1200430000, pricePerPod: 0.21, amountPods: 1200430, expiresIn: 1300000 }, { placeInLine: 12000000, pricePerPod: 0.21, amountPods: 12000, expiresIn: 1300000 }, { placeInLine: 12000000, pricePerPod: 0.21, amountPods: 12000, expiresIn: 1300000 }, { placeInLine: 12000000, pricePerPod: 0.21, amountPods: 12000, expiresIn: 1300000 }, { placeInLine: 12000000, pricePerPod: 0.21, amountPods: 12000, expiresIn: 1300000 }, { placeInLine: 12000000, pricePerPod: 0.21, amountPods: 12000, expiresIn: 1300000 }, { placeInLine: 12000434000, pricePerPod: 0.21, amountPods: 12004340, expiresIn: 1300000 }]);
     const [loading, setLoading] = React.useState(false);
     const [order, setOrder] = React.useState(false);
+    const [showFilter, setShowFilter] = React.useState(false);
     const [showModal, setShowModal] = React.useState(false);
     const [currentPage, setCurrentPage] = React.useState(1);
     const [plotsPerPage] = React.useState(4);
@@ -15,6 +18,17 @@ export default function Marketplace() {
     const indexOffLastPost = currentPage * plotsPerPage;
     const indexOfFirstPost = indexOffLastPost - plotsPerPage;
     const currentPlots = plots.slice(indexOfFirstPost, indexOffLastPost);
+    const [selectedValue, setSelectedValue] = React.useState('a');
+
+    const smallLabels = {
+      display: 'inline-block',
+      fontFamily: 'Futura-PT-Book',
+      fontSize: 'calc(9px + 0.7vmin)',
+      marginLeft: '13px',
+      textAlign: 'left' as const,
+      textTransform: 'uppercase' as const,
+      width: 'calc(100% - 13px)',
+    };
 
     const showSellModal = () => {
       setShowModal(!showModal);
@@ -35,7 +49,7 @@ export default function Marketplace() {
     };
 
     const SellModal = ({ pods }) => (
-      <div style={{ width: '100vw', height: '100vh', display: 'flex', background: 'rgb(0, 0, 0, 0.9)', zIndex: '99999', position: 'fixed', top: '0%', left: '0%', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ fontFamily: 'Futura-PT-Book', fontSize: 'calc(9px + 0.5vmin)', width: '100vw', height: '100vh', display: 'flex', background: 'rgb(0, 0, 0, 0.9)', zIndex: '99999', position: 'fixed', top: '0%', left: '0%', alignItems: 'center', justifyContent: 'center' }}>
         <div style={{ backgroundColor: 'rgb(255, 222, 151, 0.8)', padding: '20px', borderRadius: '20px', margin: '20px' }}>
           <div>
             <p>SELLING</p>
@@ -107,49 +121,27 @@ export default function Marketplace() {
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
+    const toggleFilter = () => setShowFilter(!showFilter);
+
+    const handleRadioChange = (event) => {
+      setSelectedValue(event.target.value);
+    };
+
     React.useEffect(() => {
       setLoading(false);
       setShowModal(false);
     }, [plots, setPlots]);
 
     return (
-      <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ fontFamily: 'Futura-PT-Book', fontSize: 'calc(9px + 0.5vmin)', width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', overflowX: 'hidden', overflowY: 'scroll', marginTop: '120px', paddingBottom: '100px' }}>
         <div style={{ backgroundColor: 'rgb(255, 222, 151, 0.8)', padding: '20px', borderRadius: '20px', margin: '20px' }}>
           <div>
-            <p>My Plots</p>
+            <Box style={smallLabels}>My Plots</Box>
           </div>
           <div>
-            <table cellPadding={0} cellSpacing={0}>
-              <tr>
-                <th>Place in Line</th>
-                <th>Price Per Pod</th>
-                <th>Amount of pods</th>
-                <th>Expires In</th>
-              </tr>
-              {
-                  plots.map((plot) => (
-                    <tr>
-                      <td>{plot.placeInLine}</td>
-                      <td>{plot.pricePerPod}</td>
-                      <td>{plot.amountPods}</td>
-                      <td>{plot.expiresIn}</td>
-                      <td>
-                        <Button onClick={() => setPlotToSell(plot.amountPods)}>SELL</Button>
-                      </td>
-                    </tr>
-                  ))
-              }
-            </table>
-          </div>
-        </div>
-
-        <div style={{ backgroundColor: 'rgb(255, 222, 151, 0.8)', padding: '20px', borderRadius: '20px', margin: '20px' }}>
-          <div>
-            <p>Plots</p>
-          </div>
-          <div>
-            <table cellPadding={0} cellSpacing={0}>
-              <tr>
+            <table cellPadding={0} cellSpacing={0} style={{ borderCollapse: 'collapse' }}>
+              <tr style={{ borderBottom: '2px solid black' }}>
+                <th>{' '}</th>
                 <th><Button onClick={() => OrderByPlace()}>Place in Line</Button></th>
                 <th><Button onClick={() => OrderByPrice()}>Price Per Pod</Button></th>
                 <th><Button onClick={() => OrderByPods()}>Amount of pods</Button></th>
@@ -159,20 +151,66 @@ export default function Marketplace() {
                 loading ?
                   <div>loading</div>
                   :
-                  <Plots plots={currentPlots} loading={loading} />
+                  <SellPlots plots={plots} loading={loading} />
+              }
+            </table>
+            <Button onClick={() => setPlotToSell(0)}>SELL</Button>
+          </div>
+        </div>
+
+        <Box style={{ backgroundColor: 'rgb(255, 222, 151, 0.8)', padding: '20px', borderRadius: '20px', margin: '20px' }}>
+          <div>
+            <Box style={smallLabels}>Plots</Box>
+          </div>
+          <div style={{ fontFamily: 'Futura-PT-Book', fontSize: 'calc(9px + 0.5vmin)' }}>
+            <Button onClick={() => toggleFilter()}>Filter</Button>
+          </div>
+          {
+            showFilter ?
+              <div style={{ display: 'flex', flexDirection: 'row' }}>
+                <DropDown />
+                <TextField
+                  className="TextField-rounded"
+                  variant="outlined"
+                  type="text"
+                  size="medium"
+                  placeholder="0" />
+              </div>
+            :
+            null
+          }
+          <div>
+            <table cellPadding={0} cellSpacing={0} style={{ borderCollapse: 'collapse' }}>
+              <tr style={{ borderBottom: '2px solid black' }}>
+                <th>{' '}</th>
+                <th><Button onClick={() => OrderByPlace()}>Place in Line</Button></th>
+                <th><Button onClick={() => OrderByPrice()}>Price Per Pod</Button></th>
+                <th><Button onClick={() => OrderByPods()}>Amount of pods</Button></th>
+                <th><Button onClick={() => OrderExpireDate()}>Expires In</Button></th>
+              </tr>
+              {
+                loading ?
+                  <div>loading</div>
+                  :
+                  <Plots selectedValue={selectedValue} plots={currentPlots} loading={loading} />
               }
             </table>
           </div>
           <div>
-            <Pagination plotsPerPage={plotsPerPage} totalPlots={plots.length} paginate={paginate} />
+            <Button onClick={() => console.log('buy')}>BUY</Button>
           </div>
-        </div>
-        {
-          showModal ?
-            <SellModal />
-            :
-            null
-        }
+          <div>
+            <Pagination handleRadioChange={handleRadioChange} plotsPerPage={plotsPerPage} totalPlots={plots.length} paginate={paginate} />
+          </div>
+        </Box>
+        <>
+          {
+            showModal ?
+              <SellModal />
+              :
+              null
+          }
+        </>
       </div>
     );
 }
--- src/components/SellPlots/index.tsx
+++ src/components/SellPlots/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Radio from '@mui/material/Radio';
 
-const Plots = ({ plots, selectedValue, handleRadioChange }) => {
+const SellPlots = ({ plots }) => {
   const MoneyFormat = (labelValue: number) => (Math.abs(Number(labelValue)) >= 1.0e+9 ? `${Math.abs(Number(labelValue)) / 1.0e+9}B` : Math.abs(Number(labelValue)) >= 1.0e+6 ? `${Math.abs(Number(labelValue)) / 1.0e+6}M` : Math.abs(Number(labelValue)) >= 1.0e+3 ? `${Math.abs(Number(labelValue)) / 1.0e+3}K` : Math.abs(Number(labelValue)));
   return (
     <>
       {
       plots.map((plot: any) => (
         <tr key={plot.expiresIn} style={{ borderBottom: '1px solid black' }}>
-          <td>
-            <Radio name="radio-buttons" onChange={handleRadioChange} value={`${plot.placeInLine}`} color="default" checked={selectedValue === `${plot.placeInLine}`} />
-          </td>
+          <td><Radio color="default" /></td>
           <td>{MoneyFormat(plot.placeInLine)}</td>
           <td>${plot.pricePerPod}</td>
           <td>{plot.amountPods}</td>
@@ -22,4 +20,4 @@ const Plots = ({ plots, selectedValue, handleRadioChange }) => {
   );
 };
 
-export default Plots;
+export default SellPlots;
