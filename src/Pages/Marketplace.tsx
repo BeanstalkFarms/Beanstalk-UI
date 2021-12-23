d8722f778c951384258357c5e5bd9b824e9b1311
Lawrence Wu <lawrence@lawrencewu.me>
2021-12-23 01:31:55 -0500

    buy offers

--- src/Pages/Marketplace.tsx
+++ src/Pages/Marketplace.tsx
@@ -13,22 +13,50 @@ export default function Marketplace() {
   console.log('got listings, buyOffers:', listings, buyOffers);
 
   return (
-    <div style={{ marginTop: 128, width: '100%', padding: 24, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ width: '100%', padding: 24, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ background: 'lightgray', maxWidth: 960, marginTop: 128 }}>
         {/* Listings */}
         <h1 style={{ fontSize: 24 }}>Listings</h1>
         <div>
           <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr' }}>
             <p>Seller</p>
-            <p>Seller</p>
-            <p>Seller</p>
-            <p>Seller</p>
-            <p>Seller</p>
-            <p>Seller</p>
+            <p>Index</p>
+            <p>Price / pod</p>
+            <p>Expiry</p>
+            <p>Amount</p>
+            <p>Status</p>
           </div>
           {listings.map((listing) => (
-            <div>
-              <p>{JSON.stringify(listing)}</p>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr' }}>
+              <p>{listing.listerAddress}</p>
+              <p>{listing.objectiveIndex.toString()}</p>
+              <p>{listing.pricePerPod.toString()}</p>
+              <p>{listing.expiry.toString()}</p>
+              <p>{listing.initialAmount.minus(listing.amountSold).toString()}</p>
+              <p>{listing.status}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div style={{ background: 'lightgray', maxWidth: 960, marginTop: 128 }}>
+        {/* Offers */}
+        <h1 style={{ fontSize: 24 }}>Buy Offers</h1>
+        <div>
+          <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr' }}>
+            <p>Buyer</p>
+            <p>Amount</p>
+            <p>Price / pod</p>
+            <p>Max Place in Line</p>
+            <p>Status</p>
+          </div>
+          {buyOffers.map((buyOffer) => (
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr' }}>
+              <p>{buyOffer.listerAddress}</p>
+              <p>{buyOffer.initialAmountToBuy.minus(buyOffer.amountBought.toString()}</p>
+              <p>{buyOffer.pricePerPod.toString()}</p>
+              <p>{buyOffer.maxPlaceInLine.toString()}</p>
+              <p>{buyOffer.maxPlaceInLine.toString()}</p>
+              <p>{buyOffer.status}</p>
             </div>
           ))}
         </div>
