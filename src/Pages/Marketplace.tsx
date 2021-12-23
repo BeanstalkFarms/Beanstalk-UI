ac5f3b56e3bfaeaec4411b8b9a0dd8a3b12f2646
Lawrence Wu <lawrence@lawrencewu.me>
2021-12-23 02:24:16 -0500

    todo

--- src/Pages/Marketplace.tsx
+++ src/Pages/Marketplace.tsx
@@ -9,6 +9,19 @@ import Modal from '@mui/material/Modal';
 
 import Updater from '../state/marketplace/updater';
 
+/*
+ * TODO:
+ * 1.
+ * - Need to show your current plot listings and be able to cancel them.
+ *   - Need to filter these out of all listings + your plots to sell
+ * - Be able to create a buy offer
+ *   - Be able to show current buy offers and cancel them
+ *   - Filter them out of global buy offers too
+ *
+ * 2. Figure out better UI for this
+ *    - Hook it up with better logic for default/min/max values
+ */
+
 function SellPlotModal({
   plotIndex,
   maxPods,
