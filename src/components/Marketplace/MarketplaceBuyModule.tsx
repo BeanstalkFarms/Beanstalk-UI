7f310bf29992de95ada5dd69e502f827d464d131
Lawrence Wu <lawrence@lawrencewu.me>
2021-12-28 07:49:06 -0500

    hii

--- src/components/Marketplace/MarketplaceBuyModule.tsx
+++ src/components/Marketplace/MarketplaceBuyModule.tsx
@@ -6,7 +6,7 @@ import { List as ListIcon } from '@material-ui/icons';
 import { AppState } from 'state';
 import { updateBeanstalkBeanAllowance } from 'state/allowances/actions';
 import { BASE_SLIPPAGE } from 'constants/index';
-import { approveBeanstalkBean, SwapMode, poolForLP } from 'util/index';
+import { approveBeanstalkBean, SwapMode } from 'util/index';
 import {
   BaseModule,
   ListTable,
@@ -57,28 +57,17 @@ export default function MarketplaceBuyModule() {
   );
 
   const [section, setSection] = useState(0);
-  const [sectionInfo, setSectionInfo] = useState(0);
-  const [page, setPage] = useState(0);
-  const [isFormDisabled, setIsFormDisabled] = useState(true);
-  const [listTablesStyle, setListTablesStyle] = useState({ display: 'block' });
-
-  const poolForLPRatio = (amount: BigNumber) => {
-    if (amount.isLessThanOrEqualTo(0)) return [new BigNumber(-1), new BigNumber(-1)];
-    return poolForLP(
-      amount,
-      prices.beanReserve,
-      prices.ethReserve,
-      totalBalance.totalLP
-    );
-  };
 
   const sectionTitles = [
     'Create Buy Offer',
+    'Listings',
   ];
   const sectionTitlesDescription = [
     siloStrings.beanDeposit,
+    siloStrings.beanDeposit,
   ];
   const sectionTitlesInfoDescription = [
+    siloStrings.beanDepositsTable,
     siloStrings.beanDepositsTable,
   ];
 
@@ -94,27 +83,11 @@ export default function MarketplaceBuyModule() {
     console.log('submit buy offer here:', buyOffer);
   };
 
-  const claimLPBeans = lpReceivableBalance.isGreaterThan(0)
-    ? poolForLPRatio(lpReceivableBalance)[0]
-    : new BigNumber(0);
-
-  const beanClaimable = beanReceivableBalance
-    .plus(harvestablePodBalance)
-    .plus(poolForLPRatio(lpReceivableBalance)[0]);
-
-  const ethClaimable = claimableEthBalance.plus(
-    poolForLPRatio(lpReceivableBalance)[1]
-  );
-
   const sections = [
     <CreateBuyOfferModule
       key={0}
       beanBalance={beanBalance}
-      beanClaimableBalance={beanClaimableBalance.plus(claimLPBeans)}
       beanReserve={prices.beanReserve}
-      beanClaimable={beanClaimable}
-      ethClaimable={ethClaimable}
-      beanLPClaimableBalance={claimLPBeans}
       claimable={claimable}
       claimableEthBalance={claimableEthBalance}
       ethBalance={ethBalance}
@@ -138,6 +111,9 @@ export default function MarketplaceBuyModule() {
         section={section}
         sectionTitles={sectionTitles}
         sectionTitlesDescription={sectionTitlesDescription}
+        handleTabChange={(e, value) => {
+          setSection(value)
+        }}
       >
         {sections[section]}
       </BaseModule>
