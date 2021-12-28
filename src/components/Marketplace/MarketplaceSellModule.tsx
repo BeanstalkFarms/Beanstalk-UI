56e459fb9dcfec53caa69d52dfbf05802f0afc4b
Lawrence Wu <lawrence@lawrencewu.me>
2021-12-28 08:21:16 -0500

    asdf

--- src/components/Marketplace/MarketplaceSellModule.tsx
+++ src/components/Marketplace/MarketplaceSellModule.tsx
@@ -40,9 +40,6 @@ export default function MarketplaceSellModule() {
   } = useSelector<AppState, AppState['userBalance']>(
     (state) => state.userBalance
   );
-  const { beanstalkBeanAllowance, beanstalkLPAllowance } = useSelector<AppState, AppState['allowances']>(
-    (state) => state.allowances
-  );
   const { harvestableIndex } = useSelector<AppState, AppState['weather']>(
     (state) => state.weather
   );
@@ -50,221 +47,37 @@ export default function MarketplaceSellModule() {
   // Local state
   const [section, setSection] = useState(0);
   const [sectionInfo, setSectionInfo] = useState(0);
-  const [settings, setSettings] = useState({
-    claim: false,
-    convert: false,
-    useLP: false,
-    mode: null,
-    slippage: new BigNumber(BASE_SLIPPAGE),
-  });
-  const [page, setPage] = useState(0);
-  const [isFormDisabled, setIsFormDisabled] = useState(true);
-  const [listTablesStyle, setListTablesStyle] = useState({ display: 'block' });
 
   // Handlers
   const handleTabChange = (event, newSection) => {
-    if (newSection !== section) {
-      setSection(newSection);
-      setIsFormDisabled(true);
-      setSettings({
-        claim: false,
-        convert: false,
-        useLP: false,
-        mode: null,
-        slippage: new BigNumber(BASE_SLIPPAGE),
-      });
-    }
-  };
-  const handleTabInfoChange = (event, newSectionInfo, newPageZero) => {
-    setSectionInfo(newSectionInfo);
-    setPage(newPageZero);
-  };
-  const handlePageChange = (event, newPage) => {
-    setPage(newPage);
-  };
-  const depositRef = useRef<any>();
-  const withdrawRef = useRef<any>();
-  const claimRef = useRef<any>();
-  const handleForm = () => {
-    switch (section) {
-      case 0:
-        depositRef.current.handleForm();
-        break;
-      case 1:
-        withdrawRef.current.handleForm();
-        break;
-      case 2:
-        claimRef.current.handleForm();
-        break;
-      default:
-        break;
-    }
+    setSection(newSection)
   };
 
-  // Setup
-  if (settings.mode === null) {
-    if (lpBalance.isGreaterThan(0)) {
-      setSettings((p) => ({ ...p, mode: SwapMode.LP }));
-    } else if (beanBalance.isGreaterThan(0) && ethBalance.isGreaterThan(0)) {
-      setSettings((p) => ({ ...p, mode: SwapMode.BeanEthereum }));
-    } else if (beanBalance.isGreaterThan(0)) {
-      setSettings((p) => ({ ...p, mode: SwapMode.Bean }));
-    } else if (ethBalance.isGreaterThan(0)) {
-      setSettings((p) => ({ ...p, mode: SwapMode.Ethereum }));
-    } else if (beanBalance.isEqualTo(0) && ethBalance.isEqualTo(0)) {
-      setSettings((p) => ({ ...p, mode: SwapMode.Ethereum }));
-    }
-  }
-
-  // const claimLPBeans = lpReceivableBalance.isGreaterThan(0)
-  //   ? poolForLPRatio(lpReceivableBalance)[0]
-  //   : new BigNumber(0);
-  // const beanClaimable = beanReceivableBalance
-  //   .plus(harvestablePodBalance)
-  //   .plus(poolForLPRatio(lpReceivableBalance)[0]);
-  // const ethClaimable = claimableEthBalance.plus(
-  //   poolForLPRatio(lpReceivableBalance)[1]
-  // );
-
-  // Primary section:
-  // Sell a Plot
-  const sectionTitles = ['Sell Plot'];
+  const sectionTitles = ['Buy Offers', 'Sell Plot'];
   const sectionTitlesDescription = [
+    siloStrings.lpDeposit, // FIXME
     siloStrings.lpDeposit, // FIXME
   ];
 
-  const sendRef = useRef<any>();
-  const [toAddress, setToAddress] = useState('');
-  const [isValidAddress, setIsValidAddress] = useState(false);
   const sections = [
-    <PlotSellModule
-      key={0}
-      plots={plots}
-      hasPlots={
-        plots !== undefined &&
-        (Object.keys(plots).length > 0 ||
-          harvestablePodBalance.isGreaterThan(0))
-      }
-      index={parseFloat(harvestableIndex)}
-      fromAddress=""
-      fromToken={CryptoAsset.Bean}
-      ref={sendRef}
-      // Shared state
-      isFormDisabled={isFormDisabled}
-      setIsFormDisabled={setIsFormDisabled}
-      isValidAddress={isValidAddress}
-      setIsValidAddress={setIsValidAddress}
-      toAddress={toAddress}
-      setToAddress={setToAddress}
-      setSection={setSection}
-    />,
+    <div />,
+    <div />,
   ];
-
-  // Bottom section:
-  // List of plots
-  const sectionTitlesInfo = [];
-  const sectionsInfo = [];
-  if (plots !== undefined && (Object.keys(plots).length > 0 || harvestablePodBalance.isGreaterThan(0))) {
-    sectionsInfo.push(
-      <ListTable
-        asset={FarmAsset.Pods}
-        claimableBalance={harvestablePodBalance}
-        claimableCrates={harvestablePlots}
-        crates={plots}
-        description="Sown Plots will show up here."
-        handleChange={handlePageChange}
-        indexTitle="Place in Line"
-        index={parseFloat(harvestableIndex)}
-        page={page}
-        title="Plots"
-      />
-    );
-    sectionTitlesInfo.push('Plots');
-  }
-  const showListTablesIcon = sectionsInfo.length > 0 ? (
-    <Box
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        margin: '20px 0 -56px -4px',
-      }}
-    >
-      <IconButton
-        color="primary"
-        onClick={() => {
-          const shouldExpand = listTablesStyle.display === 'none';
-          setListTablesStyle(
-            shouldExpand ? { display: 'block' } : { display: 'none' }
-          );
-        }}
-        style={{ height: '44px', width: '44px', marginTop: '-8px' }}
-      >
-        <ListIcon />
-      </IconButton>
-    </Box>
-  ) : null;
-  const showListTables = sectionsInfo.length > 0 ? (
-    <Box style={{ ...listTablesStyle, marginTop: '61px' }}>
-      <BaseModule
-        handleTabChange={handleTabInfoChange}
-        section={sectionInfo}
-        sectionTitles={sectionTitlesInfo}
-        sectionTitlesDescription={[
-          // eslint-disable-next-line
-          'A Plot of Pods is created everytime Beans are Sown. Plots have a place in the Pod Line based on the order they were created. As Pods are harvested, your Plots will automatically advance in line. Entire Plots and sections of Plots can be transferred using the Send tab of the Field module.',
-        ]}
-        showButton={false}
-      >
-        {sectionsInfo[sectionInfo]}
-      </BaseModule>
-    </Box>
-  ) : null;
-
-  // Tweaks
-  let allowance = new BigNumber(1);
-  let setAllowance = updateBeanstalkBeanAllowance;
-  let handleApprove = approveBeanstalkBean;
-  if (settings.mode === SwapMode.Bean || settings.mode === SwapMode.BeanEthereum) {
-    allowance = beanstalkBeanAllowance;
-    if (allowance.isGreaterThan(0) && settings.useLP) {
-      allowance = beanstalkLPAllowance;
-      setAllowance = updateBeanstalkLPAllowance;
-      handleApprove = approveBeanstalkLP;
-    }
-  } else if (settings.mode === SwapMode.LP) {
-    allowance = beanstalkLPAllowance;
-    setAllowance = updateBeanstalkLPAllowance;
-    handleApprove = approveBeanstalkLP;
-  }
 
   // Render
   return (
     <>
       <BaseModule
         style={{ marginTop: '20px' }}
-        allowance={section === 0 ? allowance : new BigNumber(1)}
-        resetForm={() => {
-          setSettings({
-            ...settings,
-            mode: SwapMode.Ethereum,
-          });
-        }}
-        handleApprove={handleApprove}
-        handleForm={handleForm}
+        handleForm={() => {}}
         handleTabChange={handleTabChange}
-        isDisabled={isFormDisabled}
-        locked={section === 1 && locked}
-        lockedSeasons={lockedSeasons}
-        mode={settings.mode}
+        isDisabled={false}
         section={section}
         sectionTitles={sectionTitles}
         sectionTitlesDescription={sectionTitlesDescription}
-        setAllowance={setAllowance}
       >
         {sections[section]}
-        {showListTablesIcon}
       </BaseModule>
-      {showListTables}
     </>
   );
 }
