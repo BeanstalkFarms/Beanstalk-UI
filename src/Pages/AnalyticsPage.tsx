f94b2c9b8e90ce399f0d4d39ad56df86629faaac
publius <beanstalk.publius@protonmail.com>
2022-01-02 17:18:30 -0800

    removed compiler errors

--- src/Pages/AnalyticsPage.tsx
+++ src/Pages/AnalyticsPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Page } from 'Pages/index';
 import Charts from 'components/Charts';
-import Seasons from 'components/Seasons';
-import Balances from 'components/Balances';
 
-export default function AnalyticsPage(props) {
+export default function AnalyticsPage() {
   const sectionTitles = ['Analytics'];
   const sections = [<Charts />];
 
--- src/Pages/PegMaintenancePage.tsx
+++ src/Pages/PegMaintenancePage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Page } from 'Pages/index';
-import Charts from 'components/Charts';
 import Seasons from 'components/Seasons';
-import Balances from 'components/Balances';
 
-export default function PegMaintenancePage(props) {
+export default function PegMaintenancePage() {
   const sectionTitles = ['Peg Maintenance'];
   const sections = [<Seasons />];
 
