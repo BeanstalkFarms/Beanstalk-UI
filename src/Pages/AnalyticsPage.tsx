d4cca02300cdc89260014e3ed1cca2c0b74eff39
publius <beanstalk.publius@protonmail.com>
2022-01-02 22:37:01 -0800

    fixed error

--- src/Pages/AnalyticsPage.tsx
+++ src/Pages/AnalyticsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page } from 'Pages/index';
 import Charts from 'components/Charts';
+
 export default function AnalyticsPage() {
   const sectionTitles = ['Analytics'];
   const sections = [<Charts />];
