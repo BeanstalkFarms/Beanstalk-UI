78d62d4e6cbe1b7f613d9386206a23cf0e12c618
Cameron Kelley <cameron@kelley.vc>
2022-01-23 17:35:01 -0600

    upgrade: FundraiserUtilities

--- src/util/FundraiserUtilities.tsx
+++ src/util/FundraiserUtilities.tsx
@@ -1,13 +1,11 @@
-import { beanstalkContract, txCallback } from './index';
+import { beanstalkContract } from './index';
+import { handleCallbacks, TxnCallbacks } from './TxnUtilities';
 
-export const fund = async (id, amount, callback, completeCallback) => {
-  beanstalkContract()
-    .fund(id, amount)
-    .then((response) => {
-      callback(response.hash);
-      response.wait().then(() => {
-        completeCallback();
-        txCallback();
-      });
-    });
-};
+export const fund = async (
+  id,
+  amount,
+  onResponse: TxnCallbacks['onResponse']
+) => handleCallbacks(
+  beanstalkContract().fund(id, amount),
+  { onResponse }
+);
