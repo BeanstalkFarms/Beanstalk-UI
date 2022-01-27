fa512a1278d79fe3a811ae314f05ff848df9fbac
publius <beanstalk.publius@protonmail.com>
2022-01-27 18:15:57 -0500

    Add bip10 and hide bean.money apy

--- src/constants/bips.ts
+++ src/constants/bips.ts
@@ -9,4 +9,5 @@ export const bipsList = [
   { path: '/Bips/bip-7.html', title: 'Expanded Convert' },
   { path: '/Bips/bip-8.html', title: 'Beanstalk Q1 Budget' },
   { path: '/Bips/bip-9.html', title: 'Various Efficiency Improvements' },
+  { path: '/Bips/bip-10.html', title: 'Omniscia Retainer' },
 ];
