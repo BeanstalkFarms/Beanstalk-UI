abed46a97fa34af1716eaadfe59aaa7332ebbbf5
Tina Zheng <tina.s.zheng+github@gmail.com>
2022-02-28 21:26:52 -0800

    upgrade to cra5, use create-react-rewired to include node polyfills

---
+++ config-overrides.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    stream: require.resolve('stream-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    assert: require.resolve('assert/'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    buffer: require.resolve('buffer'),
+  };
+
+  config.resolve.alias = {
+    buffer: 'buffer',
+    stream: 'stream-browserify',
+  };
+
+  config.plugins = [
+    ...config.plugins,
+    // Work around for Buffer is undefined:
+    // https://github.com/webpack/changelog-v5/issues/10
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ];
+
+  return config;
+};
