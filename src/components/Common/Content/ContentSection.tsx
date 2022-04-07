1115952dcbf6b40a00cab63a87d940f40578cb42
Cool Bean <anoncoolbean@gmail.com>
2022-04-07 11:27:05 -0500

    Revert content section

--- src/components/Common/Content/ContentSection.tsx
+++ src/components/Common/Content/ContentSection.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Box, Link, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { theme } from 'constants/index';
 
-const useStyles = makeStyles(({
+const useStyles = makeStyles({
   appSection: {
     padding: (props: any) => props.padding,
     margin: '0px',
@@ -21,46 +21,23 @@ const useStyles = makeStyles(({
       textDecoration: 'underline',
     },
   },
-  sectionStyle: {
-      width: window.innerWidth - 64,
-      margin: '10px 0 10px 0',
-      padding: '12px',
-      color: theme.backgroundText,
-      textAlign: (props) => props.textAlign,
-      '@media (min-width: 500px)': {
-          maxWidth: '550px',
-      }
-  }
-}));
+});
 
-interface ContentSectionProps {
-  description?: string;
-  title?: string;
-  descriptionLinks?: [{text:string, href: string}];
-  children: any;
-  id: string;
-  width?: string;
-  textAlign?: string;
-  minHeight?: string;
-  style?: {}
-}
-
-const ContentSection: FC<ContentSectionProps> = ({
-  title,
+export default function ContentSection({
   description,
-  style,
   descriptionLinks,
   children,
   id,
-  textAlign,
-  minHeight,
+  marginTop,
+  padding,
+  style,
   width,
-}) => {
-  const classes = useStyles({});
+  textAlign,
+}) {
   const [shouldDisplayDescription, setshouldDisplayDescription] =
     React.useState(true);
 
-
+  const classes = useStyles({ marginTop, padding, width });
 
   React.useEffect(() => {
     // on initialize fetch is_hidden variable or default to false. if is_hidden set shouldDisplay to false
@@ -80,49 +57,66 @@ const ContentSection: FC<ContentSectionProps> = ({
     );
   };
 
+  const { innerWidth } = window;
   const descriptionSection =
     description !== undefined ? (
       <Box
         className={
           shouldDisplayDescription ? `section-description-${theme.name}` : ''
         }
+        style={
+          innerWidth > 500
+            ? {
+                maxWidth: '550px',
+                margin: '10px 0 10px 0',
+                padding: '12px',
+                color: theme.backgroundText,
+                textAlign: textAlign,
+              }
+            : {
+                width: innerWidth - 64,
+                margin: '10px 0 10px 0',
+                padding: '12px',
+                color: theme.backgroundText,
+                textAlign: textAlign,
+              }
+        }
       >
-        <Box className={classes.sectionStyle}>
-          {shouldDisplayDescription ? (
-            <>
-              {description}
-              {descriptionLinks !== undefined && descriptionLinks.map((l) => (
-                <span key={l.text}>
-                  {' '}
-                  <Link
-                    style={{ color: theme.backgroundText }}
-                    key={l.text}
-                    href={l.href}
-                    target="blank"
-                  >
-                    {l.text}
-                  </Link>
-                  .
-                </span>
-              ))}
-            </>
-            ) : null}
-          <Box
-            className={classes.hideButton}
-            role="button"
-            tabIndex={0}
-            aria-pressed="false"
-            onClick={() => handleisSectionHiddenDescription()}
-            onKeyDown={() => {}}
-          >
-            {shouldDisplayDescription ? ' Hide' : 'Show'}
-          </Box>
+        {shouldDisplayDescription ? (
+          <>
+            {description}
+            {descriptionLinks.map((l) => (
+              <span key={l.text}>
+                {' '}
+                <Link
+                  style={{ color: theme.backgroundText }}
+                  key={l.text}
+                  href={l.href}
+                  target="blank"
+                >
+                  {l.text}
+                </Link>
+                .
+              </span>
+            ))}
+          </>
+        ) : null}
+
+        <Box
+          className={classes.hideButton}
+          role="button"
+          tabIndex={0}
+          aria-pressed="false"
+          onClick={() => handleisSectionHiddenDescription()}
+          onKeyDown={() => {}}
+        >
+          {shouldDisplayDescription ? ' Hide' : 'Show'}
         </Box>
       </Box>
     ) : null;
 
   return (
-    <Box id={id} className="AppContent" sx={style}>
+    <Box id={id} className="AppContent" style={style}>
       <Grid
         container
         spacing={3}
@@ -134,13 +128,12 @@ const ContentSection: FC<ContentSectionProps> = ({
       </Grid>
     </Box>
   );
-};
+}
 
 ContentSection.defaultProps = {
-  descriptionLinks: [{ text: '', href: '' }],
+  descriptionLinks: [],
+  padding: '0px 0px',
   width: '100%',
   minHeight: '0px',
   textAlign: 'center',
 };
-
-export default ContentSection;
