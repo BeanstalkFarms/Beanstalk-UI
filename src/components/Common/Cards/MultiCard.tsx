187e58de7077ea86c6261c77855c953a83e28eb0
Silo Chad <silochad@protonmail.com>
2022-04-12 09:04:45 -0500

    bump up TabbedMarketplace

--- src/components/Common/Cards/MultiCard.tsx
+++ src/components/Common/Cards/MultiCard.tsx
@@ -1,33 +1,45 @@
 import React from 'react';
+import classNames from 'classnames';
 import makeStyles from '@mui/styles/makeStyles';
 import { Card, CardProps } from '@mui/material';
 
 import { theme } from 'constants/index';
 
 const useStyles = makeStyles(() => ({
+  // Card Root
+  // NOTE: for backwards compatibility we enforce that
+  // `MultiCard` always take up the full width of its container.
+  root: {
+    width: '100%',
+    borderRadius: '25px',
+  },
+  // "Input" cards (white background)
   input: {
     backgroundColor: theme.module.background,
-    borderRadius: '25px',
     color: theme.text,
+    // FIXME
     padding: '10px',
   },
+  // "Meta" cards (partially transparent beige background)
   meta: {
     backgroundColor: theme.module.metaBackground,
     color: theme.backgroundText,
     // FIXME
-    borderRadius: '25px',
-    boxShadow: 'none',
-    marginTop: '16px',
     padding: '10px 16px 30px 16px',
+    marginTop: '16px',
   },
 }));
 
-type CardType = "meta" | "input";
+type MultiCardType = "meta" | "input";
 
-const MultiCard : React.FC<CardProps & { type: CardType }> = ({ children, type, ...props }) => {
+const MultiCard : React.FC<CardProps & { type: MultiCardType }> = ({ children, type, ...props }) => {
   const classes = useStyles();
   return (
-    <Card elevation={7} className={classes[type]} {...props}>
+    <Card
+      elevation={type === "meta" ? 0 : 7}
+      className={classNames(classes.root, classes[type])}
+      {...props}
+    >
       {children}
     </Card>
   )
