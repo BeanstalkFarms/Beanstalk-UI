cd40de2186f5bab3e7b5a7f24b0e3feb5aa1d6c9
Silo Chad <silochad@protonmail.com>
2022-04-14 00:18:45 -0500

    Use css calc instead of innerWidth for LoadingBean position

--- src/components/App/LoadingBean.tsx
+++ src/components/App/LoadingBean.tsx
@@ -7,33 +7,27 @@ import './index.tsx';
 import {makeStyles} from "@mui/styles";
 
 const useStyles = makeStyles({
-    root: {
-        height: (props: any) => props.height - 100,
-        overflow: 'hidden',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  root: {
+    height: 'calc(100vh - 100px)',
+    overflow: 'hidden',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 })
 
 export default function LoadingBean() {
-  const { innerHeight: height } = window;
-  const props = {
-      height: height
-  }
-  const classes = useStyles(props)
-
+  const classes = useStyles();
   return (
     <Box className={classes.root}>
       <Box className="Loading-logo">
         <img
           className="svg"
           name={theme.name}
-          style={{ verticalAlign: 'middle' }}
           height="250px"
           src={BeanLogo}
-          alt="app.bean.money"
+          alt=""
         />
       </Box>
     </Box>
