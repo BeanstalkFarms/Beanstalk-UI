734cd0c658932305fa3865e893d167508725eb8f
Silo Chad <silochad@protonmail.com>
2022-04-14 15:12:22 -0500

    Add .js placeholder

---
+++ src/placeholder.js
@@ -0,0 +1,7 @@
+// Without a .js file somewhere in the src/ directory,
+// the create-react-app development server show every 
+// Typescript error. I couldn't find a way to fix this
+// with configuration (even using CRA's own env vars for this),
+// so for now we'll keep this placeholder file.
+const placeholder = '';
+export default placeholder;
