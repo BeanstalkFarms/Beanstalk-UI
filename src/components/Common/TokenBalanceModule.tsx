270c7d69993c80f70754cdc8decc8e8acf2dca27
Silo Chad <silochad@protonmail.com>
2022-04-14 13:13:36 -0500

    makeStyles: TokenBalanceModule

--- src/components/Common/TokenBalanceModule.tsx
+++ src/components/Common/TokenBalanceModule.tsx
@@ -1,23 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { useMemo } from 'react';
 import BigNumber from 'bignumber.js';
 import { Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { CryptoAsset, displayBN, displayFullBN, Token, TokenLabel } from 'util/index';
 import { DataBalanceModule, TokenTypeImageModule } from '.';
-
-const style = {
-  display: 'inline',
-  fontFamily: 'Lucida Console',
-  fontWeight: '400',
-  lineHeight: '100%',
-  margin: '2px',
-  fontSize: '11px',
-};
-const imageStyle = {
-  display: 'inline-block',
-  height: '18px',
-  marginBottom: '-5px',
-  marginLeft: '5px',
-};
 
 type TokenBalanceModuleProps = {
   isCurve: boolean;
@@ -30,24 +16,43 @@ type TokenBalanceModuleProps = {
   title?: string;
 }
 
+const useStyles = makeStyles({
+  balance: {
+    display: 'inline',
+    fontFamily: 'Lucida Console',
+    fontWeight: '400',
+    lineHeight: '100%',
+    margin: '2px',
+    fontSize: '11px',
+  },
+  tokenImage: {
+    display: 'inline-block',
+    height: '18px',
+    marginBottom: '-5px',
+    marginLeft: '5px',
+  }
+})
+
 export default function TokenBalanceModule(props: TokenBalanceModuleProps) {
-  const width = window.innerWidth;
+  const classes = useStyles();
   const tokenLabel = TokenLabel(props.token);
+
+  //
+  const isCurve = props.isCurve;
+  const width = window.innerWidth;
   const content = (width > 360) ? (
     <>
-      <h5 style={style}>{displayBN(props.balance)}</h5>
-      <TokenTypeImageModule style={imageStyle} token={props.token} />
+      <h5 className={classes.balance}>{displayBN(props.balance)}</h5>
+      <TokenTypeImageModule className={classes.tokenImage} token={props.token} />
     </>
   ) : (
-    <h5 style={style}>{displayBN(props.balance)}</h5>
+    <h5 className={classes.balance}>{displayBN(props.balance)}</h5>
   );
 
-  function displayLP(balance : [BigNumber, BigNumber]) {
-    if (props.isCurve) {
-      return `${displayBN(balance[0])} BEAN/${displayBN(balance[1])} 3CRV`;
-    }
+  const displayLP = useMemo(() => (balance : [BigNumber, BigNumber]) => {
+    if (isCurve) return `${displayBN(balance[0])} BEAN/${displayBN(balance[1])} 3CRV`;
     return `${displayBN(balance[0])} BEAN/${displayBN(balance[1])} ${TokenLabel(CryptoAsset.Ethereum)}`;
-  }
+  }, [isCurve])
 
   const balanceContent = props.balance.isGreaterThan(0)
     ? (
