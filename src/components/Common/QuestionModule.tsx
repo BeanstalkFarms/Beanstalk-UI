71fa1c59410df5d8bf854fe67b060eeb1c801d62
Silo Chad <silochad@protonmail.com>
2022-04-16 11:05:48 -0500

    simplify tooltip; remove ClickAwayListener

--- src/components/Common/QuestionModule.tsx
+++ src/components/Common/QuestionModule.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
 import { StyledTooltip } from './index';
 
 type QuestionModuleProps = {
@@ -16,50 +15,25 @@ type QuestionModuleProps = {
 }
 
 const QuestionModule : React.FC<QuestionModuleProps> = (props) => {
-  const { innerWidth: width } = window;
-  const questionStyle = {
-    display: 'inline-block',
-    margin: props.margin,
-    position: props.position !== undefined ? props.position : 'absolute',
-    ...props.style,
-  };
-
-  const [show, setShow] = useState(false);
-
-  const handleTooltipClose = () => {
-    setShow(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setShow(true);
-  };
-  const timer = width < 500 ? 3000 : 250;
-
   return (
-    <Box sx={questionStyle}>
-      <ClickAwayListener onClickAway={handleTooltipClose}>
-        <StyledTooltip
-          margin={props.marginTooltip}
-          placement={
-            props.placement !== undefined ? props.placement : 'right'
-          }
-          title={props.description}
-          width={props.widthTooltip}
-          onClose={handleTooltipClose}
-          disableFocusListener
-          interactive="true"
-          open={show}
-          leaveDelay={timer}
-          onMouseEnter={handleTooltipOpen}
-          onMouseLeave={handleTooltipClose}
-        >
-          <HelpOutlineIcon
-            style={{ fontSize: props.fontSize }}
-            width="100%"
-            onClick={handleTooltipOpen}
-          />
-        </StyledTooltip>
-      </ClickAwayListener>
+    <Box sx={{
+      display: 'inline-block',
+      margin: props.margin, // || '-8px 0 0 2px',
+      position: props.position !== undefined ? props.position : 'absolute',
+      ...props.style,
+    }}>
+      <StyledTooltip
+        margin={props.marginTooltip}
+        placement={
+          props.placement !== undefined ? props.placement : 'right'
+        }
+        title={props.description}
+      >
+        <HelpOutlineIcon
+          style={{ fontSize: props.fontSize }}
+          width="100%"
+        />
+      </StyledTooltip>
     </Box>
   );
 };
