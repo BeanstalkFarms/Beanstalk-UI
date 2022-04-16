0daa65a4c0d284c031ea7d2de3a54978ead980a9
Silo Chad <silochad@protonmail.com>
2022-04-16 09:23:06 -0500

    hide console.debug in production

--- .babelrc.js
+++ .babelrc.js
@@ -18,6 +18,13 @@ const plugins = [
     },
     'icons',
   ],
+  [
+    'babel-plugin-transform-remove-console',
+    {
+      // During production build, hide `console.debug` calls.
+      "exclude": ["error", "warn", "log"]
+    }
+  ]
 ];
 
 module.exports = { plugins };
