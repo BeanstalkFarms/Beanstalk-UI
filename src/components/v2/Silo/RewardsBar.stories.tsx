cc00640e81dc671ae02a7e23eb5e14d30d97dc78
Silo Chad <silochad@protonmail.com>
2022-05-23 15:52:29 -0500

    fix broken stories

--- src/components/v2/Silo/RewardsBar.stories.tsx
+++ src/components/v2/Silo/RewardsBar.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import RewardsBar from './RewardsBar';
+import BigNumber from 'bignumber.js';
 
 export default {
   component: RewardsBar,
@@ -8,7 +9,23 @@ export default {
 } as ComponentMeta<typeof RewardsBar>;
 
 const Template: ComponentStory<typeof RewardsBar> = (args: any) => (
-  <RewardsBar {...args} />
+  <RewardsBar
+    {...args}
+    beans={{
+      earned: new BigNumber(100),
+    }}
+    stalk={{
+      total: new BigNumber(100),
+      active: new BigNumber(100),
+      earned: new BigNumber(100),
+      grown: new BigNumber(100),
+    }}
+    seeds={{
+      total: new BigNumber(100),
+      active: new BigNumber(100),
+      earned: new BigNumber(100),
+    }}
+  />
 );
 
 export const Default = Template.bind({});
--- src/components/v2/Silo/TokenTable.stories.tsx
+++ src/components/v2/Silo/TokenTable.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import TokenTable from './TokenTable';
+import { SupportedChainId } from 'constants/chains';
+import { BEAN, BEAN_ETH_UNISWAP_V2_LP } from 'constants/v2/tokens';
+import BigNumber from 'bignumber.js';
 
 export default {
   component: TokenTable,
@@ -8,7 +11,27 @@ export default {
 } as ComponentMeta<typeof TokenTable>;
 
 const Template: ComponentStory<typeof TokenTable> = (args: any) => (
-  <TokenTable {...args} />
-  );
+  <TokenTable
+    {...args}
+    config={{
+      whitelist: [
+        BEAN[SupportedChainId.MAINNET],
+        BEAN_ETH_UNISWAP_V2_LP[SupportedChainId.MAINNET]
+      ]
+    }}
+    data={{
+      tokens: {
+        [BEAN[SupportedChainId.MAINNET].address]: {
+          // input fake data here
+          deposited: new BigNumber(100_000)
+        },
+        [BEAN_ETH_UNISWAP_V2_LP[SupportedChainId.MAINNET].address]: {
+          // input fake data here
+          deposited: new BigNumber(0.0001)
+        },
+      }
+    }}
+  />
+);
 
 export const Default = Template.bind({});
