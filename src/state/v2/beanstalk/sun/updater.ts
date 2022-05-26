be1eb2be4b95aff6c7772e3ee795dda794d251a3
Silo Chad <silochad@protonmail.com>
2022-05-26 14:56:38 -0500

    temp: use season timer instead of unpause timer

--- src/state/v2/beanstalk/sun/updater.ts
+++ src/state/v2/beanstalk/sun/updater.ts
@@ -47,7 +47,8 @@ const SunUpdater = () => {
   const { activeChain } = useNetwork();
   const [fetch] = useSun();
   const dispatch = useDispatch();
-  const { awaiting, next } = useSelector<AppState, AppState['_beanstalk']['sun']['sunrise']>((state) => state._beanstalk.sun.sunrise);
+  const { season, sunrise } = useSelector<AppState, AppState['_beanstalk']['sun']>((state) => state._beanstalk.sun);
+  const { awaiting, next } = sunrise;
 
   // Update sunrise timer
   useEffect(() => {
@@ -65,9 +66,10 @@ const SunUpdater = () => {
   }, [dispatch, awaiting, next]);
 
   // 
-  // useEffect(() => {
-    
-  // }, [season])
+  useEffect(() => {
+    dispatch(setAwaitingSunrise(false));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [season]);
 
   // Fetch when chain changes
   useEffect(() => {
