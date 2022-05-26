4fa88aaba86bc3e8c2b944560c4cb7a45d8672db
Cool Bean <anoncoolbean@gmail.com>
2022-05-26 16:11:41 -0500

    Fix overflow button

--- src/components/v2/Balances/Modules/RewardsCard.tsx
+++ src/components/v2/Balances/Modules/RewardsCard.tsx
@@ -16,7 +16,7 @@ const RewardsCard: React.FC<RewardsCardProps> = ({title}) => {
   const classes = useStyles();
 
   return (
-    <Card sx={{p: 2, overflow: "visible"}}>
+    <Card sx={{p: 2}}>
       <Grid container direction="row" justifyContent="space-between" spacing={2}>
         <Grid item xs={12} md={4.5}>
           <Stack>
@@ -54,49 +54,35 @@ const RewardsCard: React.FC<RewardsCardProps> = ({title}) => {
                   <Typography>2000</Typography>
                 </Stack>
               </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography>Unclaimed Beans from Fertilizer</Typography>
-                <Stack direction="row" alignItems="center" gap={0.2}>
-                  <Typography>+</Typography>
-                  <img alt="" src={beanCircleIcon} height="18px"/>
-                  <Typography>2000</Typography>
-                </Stack>
-              </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography>Unclaimed Beans from Fertilizer</Typography>
-                <Stack direction="row" alignItems="center" gap={0.2}>
-                  <Typography>+</Typography>
-                  <img alt="" src={beanCircleIcon} height="18px"/>
-                  <Typography>2000</Typography>
-                </Stack>
-              </Stack>
             </Stack>
           </Stack>
         </Grid>
         <Grid item xs={12} md={4.5}>
-          <Typography variant="h2">Active Rewards</Typography>
-          <Stack gap={0.5} sx={{pt: 1}} justifyContent="space-between" height="100%">
-            <Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography>Earned Beans</Typography>
-                <Stack direction="row" alignItems="center" gap={0.2}>
-                  <Typography>+</Typography>
-                  <img alt="" src={beanCircleIcon} height="18px"/>
-                  <Typography>2000</Typography>
+          <Stack height="100%">
+            <Typography variant="h2">Active Rewards</Typography>
+            <Stack gap={0.5} sx={{pt: 1}} justifyContent="space-between" height="100%">
+              <Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography>Earned Beans</Typography>
+                  <Stack direction="row" alignItems="center" gap={0.2}>
+                    <Typography>+</Typography>
+                    <img alt="" src={beanCircleIcon} height="18px"/>
+                    <Typography>2000</Typography>
+                  </Stack>
+                </Stack>
+                <Stack direction="row" justifyContent="space-between">
+                  <Typography>Earned Stalk</Typography>
+                  <Stack direction="row" alignItems="center" gap={0.2}>
+                    <Typography>+</Typography>
+                    <img alt="" src={stalkIcon} height="18px"/>
+                    <Typography>2000</Typography>
+                  </Stack>
                 </Stack>
               </Stack>
-              <Stack direction="row" justifyContent="space-between">
-                <Typography>Earned Stalk</Typography>
-                <Stack direction="row" alignItems="center" gap={0.2}>
-                  <Typography>+</Typography>
-                  <img alt="" src={stalkIcon} height="18px"/>
-                  <Typography>2000</Typography>
-                </Stack>
-              </Stack>
+              <Button sx={{p: 1}}>
+                Claim all Rewards
+              </Button>
             </Stack>
-            <Button sx={{p: 1}}>
-              Claim all Rewards
-            </Button>
           </Stack>
         </Grid>
       </Grid>
