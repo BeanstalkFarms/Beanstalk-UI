5f980760b85453f3ed031eb91da10fdc2f06928b
Silo Chad <silochad@protonmail.com>
2022-05-28 08:26:44 -0500

    fertilizer item with data; get chain data on barn raise status

---
+++ src/components/v2/BarnRaise/Form.tsx
@@ -0,0 +1,107 @@
+import React, { useCallback } from 'react';
+import { Button, Card, Stack, Typography } from '@mui/material';
+import BigNumber from 'bignumber.js';
+import gearIcon from 'img/gear.svg';
+import TokenInputField from '../Common/Form/TokenInputField';
+import { Token } from 'classes';
+import { ERC20Token, NativeToken } from 'classes/Token';
+import { displayBN } from 'util/index';
+import { TokensByAddress } from 'constants/v2/tokens';
+import { BalanceState } from 'state/v2/farmer/balances/reducer';
+import PurchaseDropdown from './PurchaseFertilizer/PurchaseDropdown';
+
+export interface BarnraiseFormProps {
+  amount: BigNumber;
+  handleSetAmount: (val?: string | BigNumber) => void;
+  from: NativeToken | ERC20Token;
+  handleSetFrom: (val?: any) => void; // TODO: Add type
+  erc20TokenList: TokensByAddress<Token> | never[];
+  balances: BalanceState;
+  account: any;
+}
+
+const BuyFertilizerForm: React.FC<BarnraiseFormProps> = ({
+  amount,
+  handleSetAmount,
+  from,
+  handleSetFrom,
+  erc20TokenList,
+  balances,
+  account
+}) => {
+  const handleMax = useCallback(() => {
+    handleSetAmount(balances[from.address]);
+  }, [handleSetAmount, balances, from]);
+
+  return (
+    <Card sx={{ p: 2 }}>
+      <Stack gap={1}>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h6">Purchase Fertilizer</Typography>
+          <Button
+            sx={{
+              p: 0,
+              backgroundColor: 'transparent',
+              '&:hover': {
+                backgroundColor: 'transparent',
+                opacity: 0.8
+              }
+            }}
+          >
+            <img alt="" src={gearIcon} width="31px" />
+          </Button>
+        </Stack>
+        <form>
+          {/* {from.address} {from.name} {balances[from.address]?.toString()} */}
+          <Stack gap={1}>
+            {/* Deposit Amount */}
+            <TokenInputField
+              amount={amount}
+              setAmount={handleSetAmount}
+              token={from}
+              setToken={(t: Token) => handleSetFrom(t as ERC20Token)}
+              tokenList={erc20TokenList}
+            />
+            {/* Max Module */}
+            {/* only show 'max' button if user's wallet is connected */}
+            {account && (
+              <Stack direction="row" alignItems="center" spacing={0.5} px={0.75}>
+                <Stack direction="row" alignItems="center" sx={{ flex: 1 }} spacing={1}>
+                  {/* {token === ETH ? (
+                    <>
+                      <Typography variant="body1" sx={{ fontSize: 13.5 }}>
+                        = {displayBN(usdcAmount)} USDC
+                      </Typography>
+                      {quoting && <CircularProgress variant="indeterminate" size="small" sx={{ width: 14, height: 14 }} />}
+                    </>
+                  ) : null} */}
+                </Stack>
+                <Typography sx={{ fontSize: 13.5 }}>
+                  Balance: {balances[from.address] ? displayBN(balances[from.address]) : '0'}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  onClick={handleMax}
+                  color="primary"
+                  sx={{ fontSize: 13.5, fontWeight: 600, cursor: 'pointer' }}
+                >
+                  (Max)
+                </Typography>
+              </Stack>
+            )}
+            {/* Output */}
+            {amount.gt(0) ? (
+              // DISPLAY PURCHASE INFO
+              <PurchaseDropdown token={from} amount={amount} />
+            ) : null}
+            <Button disabled type="submit" size="large" fullWidth>
+              Input Amount
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
+    </Card>
+  );
+};
+
+export default BuyFertilizerForm;
