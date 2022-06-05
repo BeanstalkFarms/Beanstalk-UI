755d882897c07c57e20d37b086a7c3c51fc98574
sweetredbeans <sweetredbeans@bean.farm>
2022-06-05 15:20:52 +0900

    Small Adjustments to Font & Navbar
    - added white background to top of navbar beanprice msg
    - adjusted font from text.secondary to black for small subtitles throughout the app
    - adjusted alignment of bean tokens in My Fertilizer Component

--- src/components/v2/BarnRaise/MyFertilizer.tsx
+++ src/components/v2/BarnRaise/MyFertilizer.tsx
@@ -1,5 +1,15 @@
 import React, { useMemo, useState } from 'react';
-import { Box, Card, Divider, Grid, Stack, Tab, Tabs, Tooltip, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  Divider,
+  Grid,
+  Stack,
+  Tab,
+  Tabs,
+  Tooltip,
+  Typography,
+} from '@mui/material';
 import BigNumber from 'bignumber.js';
 import { useSelector } from 'react-redux';
 import { useHumidityFromId } from 'hooks/useHumidity';
@@ -17,32 +27,37 @@ enum TabState {
   USED = 1,
 }
 
-const MyFertilizer : React.FC = () => {
-  const farmerFertilizer = useSelector<AppState, AppState['_farmer']['fertilizer']>((state) => state._farmer.fertilizer);
+const MyFertilizer: React.FC = () => {
+  const farmerFertilizer = useSelector<
+    AppState,
+    AppState['_farmer']['fertilizer']
+  >((state) => state._farmer.fertilizer);
   const getHumidity = useHumidityFromId();
   const [tab, setTab] = useState<TabState>(TabState.ACTIVE);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setTab(newValue);
   };
-  const tokenIds = useMemo(() => 
-    Object.keys(farmerFertilizer.tokens).filter(
-      () => (tab === TabState.ACTIVE)
-    ),
+  const tokenIds = useMemo(
+    () =>
+      Object.keys(farmerFertilizer.tokens).filter(
+        () => tab === TabState.ACTIVE
+      ),
     [farmerFertilizer.tokens, tab]
   );
 
   const fertilizerSummary = useMemo(
-    () => tokenIds.reduce(
-      (agg, thisId) => {
-        const [humidity] = getHumidity(); // new BigNumber(thisId);
-        const amount = farmerFertilizer.tokens[thisId];
-        agg.unfertilized = agg.unfertilized.plus(
-          amount.multipliedBy(humidity.plus(1))
-        );
-        return agg;
-      },
-      { unfertilized: new BigNumber(0) }
-    ),
+    () =>
+      tokenIds.reduce(
+        (agg, thisId) => {
+          const [humidity] = getHumidity(); // new BigNumber(thisId);
+          const amount = farmerFertilizer.tokens[thisId];
+          agg.unfertilized = agg.unfertilized.plus(
+            amount.multipliedBy(humidity.plus(1))
+          );
+          return agg;
+        },
+        { unfertilized: new BigNumber(0) }
+      ),
     [farmerFertilizer, tokenIds, getHumidity]
   );
 
@@ -52,25 +67,56 @@ const MyFertilizer : React.FC = () => {
       <Stack sx={{ p: 2 }} gap={1}>
         <Typography variant="h2">My Fertilizer</Typography>
         <Stack gap={1}>
-          <Stack direction="row" alignItems="center" justifyContent="space-between">
-            <Tooltip title="Beans owed to you based on your ownership of Fertilizer." placement="right">
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Tooltip
+              title="Beans owed to you based on your ownership of Fertilizer."
+              placement="right"
+            >
               <Typography variant="body1">
-                Total Unfertilized Beans&nbsp;<HelpOutlineIcon fontSize="14px" sx={{ color: 'text.secondary' }} />
+                Total Unfertilized Beans&nbsp;
+                <HelpOutlineIcon
+                  fontSize="14px"
+                  sx={{ color: 'text.secondary' }}
+                />
               </Typography>
             </Tooltip>
-            <Stack direction="row" alignItems="center" gap={0.2}>
-              <TokenIcon token={BEAN[SupportedChainId.MAINNET]} />&nbsp;
-              <Typography>{displayBN(fertilizerSummary.unfertilized, 0)}</Typography>
+            <Stack
+              direction="row"
+              alignItems="top"
+              gap={0.01}
+              justifyContent=""
+            >
+              <TokenIcon token={BEAN[SupportedChainId.MAINNET]} />
+              &nbsp;
+              <Typography>
+                {displayBN(fertilizerSummary.unfertilized, 0)}
+              </Typography>
             </Stack>
           </Stack>
-          <Stack direction="row" alignItems="center" justifyContent="space-between">
-            <Tooltip title="Beans earned from Fertilizer. These Beans are Claimable." placement="right">
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Tooltip
+              title="Beans earned from Fertilizer. These Beans are Claimable."
+              placement="right"
+            >
               <Typography variant="body1">
-                Total Fertilized Beans&nbsp;<HelpOutlineIcon fontSize="14px" sx={{ color: 'text.secondary' }} />
+                Total Fertilized Beans&nbsp;
+                <HelpOutlineIcon
+                  fontSize="14px"
+                  sx={{ color: 'text.secondary' }}
+                />
               </Typography>
             </Tooltip>
-            <Stack direction="row" alignItems="center" gap={0.1}>
-              <TokenIcon token={BEAN[SupportedChainId.MAINNET]} />&nbsp;
+            <Stack direction="row" alignItems="top" gap={0.01}>
+              <TokenIcon token={BEAN[SupportedChainId.MAINNET]} />
+              &nbsp;
               <Typography>0</Typography>
             </Stack>
           </Stack>
@@ -79,7 +125,12 @@ const MyFertilizer : React.FC = () => {
       <Divider />
       {/* Fertilizers */}
       <Stack sx={{ px: 2, pb: 2, pt: 1 }} spacing={0}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ py: 1 }}>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ py: 1 }}
+        >
           <Tabs value={tab} onChange={handleChange} sx={{ minHeight: 0 }}>
             <Tab label="Active" />
             <Tab label="Used" />
@@ -107,7 +158,12 @@ const MyFertilizer : React.FC = () => {
               {/* <Grid item xs={6} md={3}>test</Grid> */}
             </Grid>
           ) : (
-            <Stack direction="column" justifyContent="center" alignItems="center" gap={2}>
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              gap={2}
+            >
               <Box width={155}>
                 <FertilizerItem
                   state="used"
@@ -116,8 +172,13 @@ const MyFertilizer : React.FC = () => {
                   remaining={zeroBN}
                 />
               </Box>
-              <Typography variant="body2" textAlign="center" sx={{ maxWidth: { md: 400 } }}>
-                Purchase Fertilizer using the module above to receive interest at the specified Humidity in the form of future Bean mints.
+              <Typography
+                variant="body2"
+                textAlign="center"
+                sx={{ maxWidth: { md: 400 } }}
+              >
+                Purchase Fertilizer using the module above to receive interest
+                at the specified Humidity in the form of future Bean mints.
               </Typography>
             </Stack>
           )}
