98753537c075a09248855c4151a3f33a9b11ca2a
Cool Bean <anoncoolbean@gmail.com>
2022-06-14 12:54:32 -0500

    Add TWAP & Pod Rate Calculations

--- src/pages/forecast.tsx
+++ src/pages/forecast.tsx
@@ -11,10 +11,12 @@ import Stat from 'components/Common/Stat';
 import TokenIcon from 'components/Common/TokenIcon';
 import { BEAN } from 'constants/tokens';
 import BigNumber from 'bignumber.js';
-import { displayFullBN } from '../util';
+import { useSelector } from 'react-redux';
+import { displayBN, displayFullBN } from '../util';
 import { ANALYTICS_LINK, SupportedChainId } from '../constants';
 import SiloBalances from '../components/Common/SiloBalances';
 import useBeanstalkSiloBreakdown from '../hooks/useBeanstalkSiloBreakdown';
+import { AppState } from '../state';
 
 const ForecastPage: React.FC = () => {
   const [tab, setTab] = useState(0);
@@ -23,6 +25,16 @@ const ForecastPage: React.FC = () => {
   };
   const breakdown = useBeanstalkSiloBreakdown();
   const chainId = useChainId();
+  
+  const beanPrice = useSelector<AppState, AppState['_bean']['token']['price']>(
+    (state) => state._bean.token.price
+  );
+  const { season } = useSelector<AppState, AppState['_beanstalk']['sun']>((state) => state._beanstalk.sun);
+  const { totalPods } = useSelector<AppState, AppState['_beanstalk']['field']>((state) => state._beanstalk.field);
+  const { supply: totalBeanSupply } = useSelector<AppState, AppState['_bean']['token']>((state) => state._bean.token);
+  const podRate = totalPods.dividedBy(totalBeanSupply).multipliedBy(100);
+
+  const isPriceLoading = beanPrice.eq(new BigNumber(-1));
 
   return (
     <Container maxWidth="lg">
@@ -55,9 +67,10 @@ const ForecastPage: React.FC = () => {
               <Stat
                 title="Time Weighted Average Price"
                 color="primary"
-                amount={`$${displayFullBN(breakdown.totalValue.abs(), 2)}`}
+                amount={`$${(isPriceLoading ? 0.0000 : beanPrice).toFixed(4)}`}
                 icon={undefined}
                 topIcon={<TokenIcon token={BEAN[SupportedChainId.MAINNET]} />}
+                bottomText={`Season ${displayBN(season)}`}
               />
               <Box>
                 <Typography>Last cross: 2m ago</Typography>
@@ -69,13 +82,11 @@ const ForecastPage: React.FC = () => {
             <Stack direction="row" justifyContent="space-between" sx={{ p: 2 }}>
               <Stat
                 title="Pod Rate"
-                amount={`${displayFullBN(breakdown.totalValue.abs(), 2)}%`}
+                amount={`${displayBN(podRate)}%`}
                 icon={undefined}
                 topIcon={<TokenIcon token={BEAN[SupportedChainId.MAINNET]} />}
+                bottomText={`Season ${displayBN(season)}`}
               />
-              <Box>
-                <Typography>Last cross: 2m ago</Typography>
-              </Box>
             </Stack>
             <Typography>test</Typography>
           </Card>
@@ -87,7 +98,7 @@ const ForecastPage: React.FC = () => {
           </Tabs>
           <Stat
             title="Total Beanstalk Liquidity"
-            amount={`$${displayFullBN(new BigNumber(1000000))}`}
+            amount={`$${displayFullBN(breakdown.totalValue.abs(), 2)}`}
             icon={undefined}
           />
           <Box sx={{ display: tab === 0 ? 'block' : 'none' }}>
