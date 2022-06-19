49f73dc544875469e30fb4455a48f9699df7298c
Cool Bean <anoncoolbean@gmail.com>
2022-06-19 14:51:50 -0500

    Update nav to design spec

--- src/components/Nav/routes.ts
+++ src/components/Nav/routes.ts
@@ -1,3 +1,11 @@
+import aboutIcon from 'img/nav-icons/about.svg';
+import beanNFTIcon from 'img/nav-icons/bean-nft.svg';
+import discordIcon from 'img/nav-icons/discord.svg';
+import githubIcon from 'img/nav-icons/github.svg';
+import governanceIcon from 'img/nav-icons/governance.svg';
+import tradeIcon from 'img/nav-icons/trade.svg';
+import twitterIcon from 'img/nav-icons/twitter.svg';
+
 const ROUTES = {
   top: [
     {
@@ -12,10 +20,6 @@ const ROUTES = {
     {
       path: '/field',
       title: 'Field',
-    },
-    {
-      path: '/balances',
-      title: 'Balances',
     },
     {
       path: '/forecast',
@@ -50,35 +54,58 @@ const ROUTES = {
   ],
   more: [
     {
-      path: 'trade',
-      title: 'Silo',
-      disabled: true,
-    },
-    {
-      path: 'beanfts',
-      title: 'BeaNFTs',
-      disabled: true,
-    },
-    {
       path: 'governance',
       href: 'https://snapshot.org/#/beanstalkdao.eth',
       title: 'Governance',
+      icon: governanceIcon
     },
     {
       path: 'analytics',
       href: 'https://dune.xyz/tbiq/Beanstalk',
       title: 'Analytics',
+      icon: beanNFTIcon
+    },
+    {
+      path: 'beanfts',
+      title: 'BeaNFTs',
+      disabled: true,
+      small: true,
+      icon: beanNFTIcon
+    },
+    {
+      path: 'trade',
+      title: 'Trade',
+      disabled: true,
+      small: true,
+      icon: tradeIcon
+    },
+  ],
+  additional: [
+    {
+      path: 'about',
+      title: 'About',
+      href: 'https://bean.money',
+      disabled: false,
+      icon: aboutIcon
     },
     {
       path: 'discord',
       href: 'https://discord.gg/beanstalk',
       title: 'Discord',
+      icon: discordIcon
     },
     {
-      path: 'about',
-      title: 'About',
-      href: 'https://bean.money',
+      path: 'twitter',
+      href: 'https://twitter.com/beanstalkfarms',
+      title: 'Twitter',
+      icon: twitterIcon
     },
+    {
+      path: 'github',
+      href: 'https://github.com/beanstalkfarms',
+      title: 'Github',
+      icon: githubIcon
+    }
   ],
 };
 
