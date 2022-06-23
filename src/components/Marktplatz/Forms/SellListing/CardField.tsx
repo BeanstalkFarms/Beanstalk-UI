7135e98c012d2e48d18d78cdfd2f3210aa37bda5
Silo Chad <silochad@protonmail.com>
2022-06-23 00:19:05 -0500

    add examples

--- src/components/Marktplatz/Forms/SellListing/CardField.tsx
+++ src/components/Marktplatz/Forms/SellListing/CardField.tsx
@@ -5,34 +5,49 @@ import { SellListingFormValues } from '../../Modals/SellListingModal';
 import { BeanstalkPalette } from '../../../App/muiTheme';
 
 export type CardFieldProps = {
-  title: string;
-  value: string;
-  setValue: any;
-  values: SellListingFormValues;
+  name: string;
+  options: ({ title: string; description: string; value: any; })[];
 }
 
-const CardField: React.FC<CardFieldProps & SelectProps> =
-  ({
-     title,
-     value,
-     values,
-     setValue
-   }) => {
-    const handleSetFieldValue = () => {
-      setValue('card', value);
-    };
-    return (
-      <Field value={value}>
-        {(fieldProps: FieldProps) => (
-          <Card
-            sx={{ cursor: 'pointer', backgroundColor: values.card === value ? BeanstalkPalette.lightishGrey : null }}
-            onClick={handleSetFieldValue}
-          >
-            <Typography>{title}</Typography>
-          </Card>
-        )}
-      </Field>
-    );
-  };
+const CardField: React.FC<CardFieldProps & SelectProps> = ({
+  name,
+  options,
+}) => {
+  // const handleSetFieldValue = () => {
+  //   setValue('card', value);
+  // };
+  return (
+    <Field name={name}>
+      {(fieldProps: FieldProps) => (
+        <div>
+          {options.map((opt) => (
+            <Card
+              key={opt.value}
+              sx={{
+                cursor: 'pointer',
+                backgroundColor: fieldProps.field.value === opt.value ? BeanstalkPalette.lightishGrey : null
+              }}
+              onClick={() => {
+                fieldProps.form.setFieldValue(name, opt.value);
+              }}
+            >
+              <Typography>{opt.title}</Typography>
+              <Typography>{opt.description}</Typography>
+            </Card>
+          ))}
+        </div>
+        // <Card
+        //   sx={{
+        //     cursor: 'pointer',
+        //     // backgroundColor: values.card === value ? BeanstalkPalette.lightishGrey : null
+        //   }}
+        //   onClick={handleSetFieldValue}
+        // >
+        //   <Typography>{title}</Typography>
+        // </Card>
+      )}
+    </Field>
+  );
+};
 
 export default CardField;
