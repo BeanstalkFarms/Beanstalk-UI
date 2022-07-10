dffbb271435d457a37f6e4cbac058ffa5172eb55
Silo Chad <silochad@protonmail.com>
2022-07-09 20:28:42 -0500

    fix deps

---
+++ src/graph/index.ts
@@ -0,0 +1,4 @@
+export * from './v1/UniswapV2Graph';
+export * from './v1/BeanSubgraph';
+export * from './BeanNFTGraph';
+export * from './v1/BeanstalkSubgraph';
--- src/graph/v1/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './UniswapV2Graph';
-export * from './BeanSubgraph';
-export * from './BeanNFTGraph';
-export * from './BeanstalkSubgraph';
