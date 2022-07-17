1d71adc98f0c50b4f3c15ff7ac537137a867e22e
Silo Chad <silochad@protonmail.com>
2022-07-17 10:44:21 -0500

    only lint staged files

--- src/util/Account.ts
+++ src/util/Account.ts
@@ -2,6 +2,8 @@ export let getAccount = (account: string) => account.toLowerCase();
 
 if (process.env.NODE_ENV === 'development' 
     && process.env.REACT_APP_OVERRIDE_FARMER_ACCOUNT) {
-      console.warn(`Using overridden Farmer account: ${process.env.REACT_APP_OVERRIDE_FARMER_ACCOUNT}`);
+      console.warn(
+        `Using overridden Farmer account: ${process.env.REACT_APP_OVERRIDE_FARMER_ACCOUNT}`
+      );
       getAccount = () => (process.env.REACT_APP_OVERRIDE_FARMER_ACCOUNT as string)!.toLowerCase();
     }
