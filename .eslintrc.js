fa26ea28e09040cdf56fb74791d55504b47822d9
Silo Chad <silochad@protonmail.com>
2022-07-17 08:23:55 -0500

    eslintrc comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,8 +45,8 @@ module.exports = {
     /// https://github.com/sweepline/eslint-plugin-unused-imports#usage
     '@typescript-eslint/no-unused-vars': 'off',
     'no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
+    'unused-imports/no-unused-imports': 'error',  // fixable
+    'unused-imports/no-unused-vars': [ // NOT auto-fixable
       'warn',
       { 
         vars: 'all',
