437d76b1de906bd623a36dc5e2af3e190572e23a
Silo Chad <silochad@protonmail.com>
2022-07-21 10:39:44 -0500

    fix: pod rate calc

--- src/hooks/usePodRate.ts
+++ src/hooks/usePodRate.ts
@@ -2,9 +2,9 @@ import { useSelector } from 'react-redux';
 import { AppState } from 'state';
 
 const usePodRate = () => {
-  const { totalPods } = useSelector<AppState, AppState['_beanstalk']['field']>((state) => state._beanstalk.field);
+  const { podLine } = useSelector<AppState, AppState['_beanstalk']['field']>((state) => state._beanstalk.field);
   const { supply: totalBeanSupply } = useSelector<AppState, AppState['_bean']['token']>((state) => state._bean.token);
-  return totalPods.dividedBy(totalBeanSupply).multipliedBy(100);
+  return podLine.dividedBy(totalBeanSupply).multipliedBy(100);
 };
 
 export default usePodRate;
