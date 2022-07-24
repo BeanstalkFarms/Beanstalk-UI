af4de1bae84f267f118fb7fe80d810754e93b33d
Silo Chad <silochad@protonmail.com>
2022-07-23 21:39:14 -0500

    temp: hide edit-listing and edit-order

--- src/pages/market/edit-order.tsx
+++ src/pages/market/edit-order.tsx
@@ -1,49 +1,34 @@
 import React from 'react';
-import {
-  Container,
-  Stack,
-  Typography,
-} from '@mui/material';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useAccount } from 'wagmi';
-import { mockPodOrderData } from '../../components/Market/Plots.mock';
-import OrderDetails from '../../components/Market/Cards/OrderDetails';
-import { AppState } from '../../state';
-import AddressIcon from '../../components/Common/AddressIcon';
-import { getAccount } from '../../util/Account';
-import PageHeaderSecondary from '../../components/Common/PageHeaderSecondary';
 
-const EditOrderPage: React.FC = () => {
+const EditOrderPage: React.FC = () => 
   // index of plot
   // eslint-disable-next-line
-  const { id } = useParams<{ id: string }>();
-  const { data: account } = useAccount();
+  // const { id } = useParams<{ id: string }>();
+  // const { data: account } = useAccount();
 
-  const beanstalkField = useSelector<AppState, AppState['_beanstalk']['field']>(
-    (state) => state._beanstalk.field
-  );
+  // const beanstalkField = useSelector<AppState, AppState['_beanstalk']['field']>(
+  //   (state) => state._beanstalk.field
+  // );
 
-  if (!account?.address) {
-    return null;
-  }
+  // if (!account?.address) {
+  //   return null;
+  // }
 
-  return (
-    <Container maxWidth="sm">
-      <Stack spacing={2}>
-        <PageHeaderSecondary
-          title={(
-            <Stack direction="row" gap={0.5} alignItems="center">
-              <AddressIcon address={account.address} />
-              <Typography variant="h2">{`${getAccount(account.address).substring(0, 7)}...'s Pod Order`}</Typography>
-            </Stack>
-          )}
-          returnPath="/market/account"
-        />
-        <OrderDetails podListing={mockPodOrderData[0]} harvestableIndex={beanstalkField.harvestableIndex} />
-      </Stack>
-    </Container>
-  );
-};
-
+  // return (
+  //   <Container maxWidth="sm">
+  //     <Stack spacing={2}>
+  //       <PageHeaderSecondary
+  //         title={(
+  //           <Stack direction="row" gap={0.5} alignItems="center">
+  //             <AddressIcon address={account.address} />
+  //             <Typography variant="h2">{`${getAccount(account.address).substring(0, 7)}...'s Pod Order`}</Typography>
+  //           </Stack>
+  //         )}
+  //         returnPath="/market/account"
+  //       />
+  //       <OrderDetails podListing={mockPodOrderData[0]} harvestableIndex={beanstalkField.harvestableIndex} />
+  //     </Stack>
+  //   </Container>
+  // );
+   null;
 export default EditOrderPage;
