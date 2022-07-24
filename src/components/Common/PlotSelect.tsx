310a0e70c6ef427925e27ce0eaef1c5bdaaed097
Silo Chad <silochad@protonmail.com>
2022-07-24 17:27:35 -0500

    show message if plot is already listed

--- src/components/Common/PlotSelect.tsx
+++ src/components/Common/PlotSelect.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo } from 'react';
-import { Stack, Typography, ListItem, ListItemButton, ListItemIcon, ListItemText, List } from '@mui/material';
+import { Stack, Typography, ListItem, ListItemButton, ListItemIcon, ListItemText, List, Box } from '@mui/material';
 import BigNumber from 'bignumber.js';
 import { makeStyles } from '@mui/styles';
+import { BEAN } from 'constants/tokens';
+import useFarmerListings from 'hooks/redux/useFarmerListings';
 import { BeanstalkPalette, FontSize, IconSize } from '../App/muiTheme';
-import { displayBN } from '../../util';
+import { displayBN, toStringBaseUnitBN } from '../../util';
 import podIcon from '../../img/beanstalk/pod-icon.svg';
 import { PlotMap } from '../../state/farmer/field';
 
@@ -37,57 +39,77 @@ export interface PlotSelectProps {
 
 const PlotSelect: React.FC<PlotSelectProps> = ({ plots, harvestableIndex, handlePlotSelect, selected }) => {
   const classes = useStyles();
+  const farmerListings = useFarmerListings();
   const orderedPlotKeys = useMemo(() => {
     if (!plots) return null;
     /// float sorting is good enough here
     return Object.keys(plots).sort((a, b) => parseFloat(a) - parseFloat(b));
   }, [plots]);
   if (!plots || !orderedPlotKeys) return null;
-  return (
-    <List sx={{ p: 0 }}>
-      {orderedPlotKeys.map((index) => (
-        <ListItem
-          key={index}
-          color="primary"
-          selected={selected ? selected === index : undefined}
-          disablePadding
-          onClick={() => handlePlotSelect(index)}
-          sx={{
-              // ListItem is used elsewhere so we define here
-              // instead of in muiTheme.ts
-              '& .MuiListItemText-primary': {
-                fontSize: FontSize['1xl'],
-                lineHeight: '1.875rem'
-              },
-              '& .MuiListItemText-secondary': {
-                fontSize: FontSize.base,
-                lineHeight: '1.25rem',
-                color: BeanstalkPalette.lightishGrey
-              },
-            }}
-          >
-          <ListItemButton disableRipple>
-            <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ width: '100%' }}>
-              <Stack direction="row" justifyContent="center">
-                <ListItemIcon sx={{ pr: 1 }}>
-                  <img src={podIcon} alt="" className={classes.tokenLogo} />
-                </ListItemIcon>
-                <ListItemText
-                  primary="PODS"
-                  secondary={`Place in Line: ${displayBN(new BigNumber(index).minus(harvestableIndex))}`}
-                  sx={{ my: 0 }}
-                  />
-              </Stack>
-              {plots[index] ? (
-                <Typography variant="bodyLarge">
-                  {displayBN(new BigNumber(plots[index]))}
-                </Typography>
-                ) : null}
+  
+  ///
+  let numAlreadyListed = 0;
+  const items = orderedPlotKeys.map((index) => {
+    const id = toStringBaseUnitBN(index, BEAN[1].decimals);
+    const listing = farmerListings[id];
+    if (listing) numAlreadyListed += 1;
+    return (
+      <ListItem
+        key={index}
+        color="primary"
+        selected={selected ? selected === index : undefined}
+        disablePadding
+        onClick={() => handlePlotSelect(index)}
+        sx={{
+            // ListItem is used elsewhere so we define here
+            // instead of in muiTheme.ts
+            '& .MuiListItemText-primary': {
+              fontSize: FontSize['1xl'],
+              lineHeight: '1.875rem'
+            },
+            '& .MuiListItemText-secondary': {
+              fontSize: FontSize.base,
+              lineHeight: '1.25rem',
+              color: BeanstalkPalette.lightishGrey
+            },
+          }}
+        >
+        <ListItemButton disableRipple>
+          <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ width: '100%' }}>
+            <Stack direction="row" justifyContent="center">
+              <ListItemIcon sx={{ pr: 1 }}>
+                <img src={podIcon} alt="" className={classes.tokenLogo} />
+              </ListItemIcon>
+              <ListItemText
+                primary={`PODS${listing ? '*' : ''}`}
+                secondary={`Place in Line: ${displayBN(new BigNumber(index).minus(harvestableIndex))}`}
+                sx={{ my: 0 }}
+                />
             </Stack>
-          </ListItemButton>
-        </ListItem>
-        ))}
-    </List>
+            {plots[index] ? (
+              <Typography variant="bodyLarge">
+                {displayBN(plots[index])} Pods
+              </Typography>
+              ) : null}
+          </Stack>
+        </ListItemButton>
+      </ListItem>
+    );
+  });
+  
+  return (
+    <>
+      {numAlreadyListed > 0 ? (
+        <Box px={1}>
+          <Typography color="text.secondary" fontSize="bodySmall">
+            * This Plot is already listed on the Market. Creating a new Listing with it will override the previous one.
+          </Typography>
+        </Box>
+      ) : null}
+      <List sx={{ p: 0 }}>
+        {items}
+      </List>
+    </>
   );
 };
 
