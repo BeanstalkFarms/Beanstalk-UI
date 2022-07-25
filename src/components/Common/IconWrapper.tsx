2ba38000d72262a4ae1e66f148ff65e466899a25
Cool Bean <anoncoolbean@gmail.com>
2022-07-25 13:45:43 -0500

    update comment

--- src/components/Common/IconWrapper.tsx
+++ src/components/Common/IconWrapper.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Box, BoxProps } from '@mui/material';
 
 /**
- * Sometimes we need an icon to live inside a
- * box of some fixed size.An example of this is
- * the Alert component. The icon size should be
- * 20px, but the box it lives in needs to be 25px
- * so that its icon & text aligns with the
- * Transaction Details accordion.
+ * An example of why we need IconWrapper:
+ *
+ * If a form has an Alert warning, we want the alert
+ * text and icon to align with the Transaction Details.
+ *
+ * One way to do this is to set both icon sizes to the same
+ * width and match the surrounding padding.
+ *
+ * The problem is that not all icons are 1:1 ratio, so what ends
+ * up happening is the icons visually look different in size if
+ * we just set all their widths the same.
+ *
+ * IconWrapper lets us put an icon in a fixed width box
+ * so that the alignment of (for ex) Alert and Transaction Details
+ * always match, while giving us the flexibility to freely
+ * adjust their icon sizes independently of one another.
  */
 const IconWrapper : React.FC<{ boxSize: number } & BoxProps> = ({ boxSize, children, sx }) => (
   <Box
