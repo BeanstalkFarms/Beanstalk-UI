53215cc7c6ffc2ae9a1a92d7109059149740723a
Cool Bean <anoncoolbean@gmail.com>
2022-07-24 22:52:03 -0500

    tooltip covers form field

--- src/components/Common/Form/FieldWrapper.tsx
+++ src/components/Common/Form/FieldWrapper.tsx
@@ -4,7 +4,7 @@ import { Stack, Tooltip, Typography } from '@mui/material';
 const FieldWrapper : React.FC<{ label?: ReactNode | string; tooltip?: string }> = ({ label, tooltip, children }) => (
   <Stack gap={0.25}>
     {label && (
-      <Tooltip title={tooltip !== undefined ? tooltip : ''} placement="bottom-start">
+      <Tooltip title={tooltip !== undefined ? tooltip : ''} placement="top-start">
         <Typography sx={{ fontSize: 'bodySmall', px: 1 }} display="inline">{label}</Typography>
       </Tooltip>
     )}
