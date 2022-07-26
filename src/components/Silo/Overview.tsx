0b30c1fe1ff56cc9089c8d0f41d592c0f0a2ed7c
Cool Bean <anoncoolbean@gmail.com>
2022-07-26 00:38:52 -0500

    zero state for silo chart

--- src/components/Silo/Overview.tsx
+++ src/components/Silo/Overview.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Stack, Tab, Tabs } from '@mui/material';
+import { Box, Card, Stack, Tab, Tabs, Typography } from '@mui/material';
 import BigNumber from 'bignumber.js';
 import useFarmerSiloBreakdown from 'hooks/useFarmerSiloBreakdown';
 import React, { useCallback, useEffect, useState } from 'react';
@@ -12,6 +12,8 @@ import {
 } from 'components/Common/Charts/LineChart.mock';
 import MainnetBlur from 'components/Common/MainnetBlur';
 import Stat from 'components/Common/Stat';
+import { BeanstalkPalette } from '../App/muiTheme';
+import WalletButton from '../Common/Connection/WalletButton';
 
 // ------------------------------------------------
 
@@ -48,7 +50,10 @@ const DepositsTab: React.FC<TabData> = ({ season, current, series }) => {
       </Box>
       <Box sx={{ width: '100%', height: '200px', position: 'relative' }}>
         <MainnetBlur>
-          Historical Deposit value will be available once Beanstalk is Replanted.
+          <Stack justifyContent="center" alignItems="center" gap={1}>
+            <Typography variant="body1" color={BeanstalkPalette.lightishGrey}>Your Silo Deposits will appear here.</Typography>
+            <WalletButton color="primary" sx={{ height: 45 }} />
+          </Stack>
         </MainnetBlur>
         <LineChart series={series} onCursor={handleCursor} />
       </Box>
