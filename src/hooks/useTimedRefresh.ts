a00e9f086df92814590c095f279e82eba1edeb7e
Silo Chad <silochad@protonmail.com>
2022-07-28 00:36:45 -0700

    clear interval after teardown

--- src/hooks/useTimedRefresh.ts
+++ src/hooks/useTimedRefresh.ts
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useRef } from 'react';
 const useTimedRefresh = (handler: () => any, intervalMs : number, enabled : boolean = true) => {
   const interval = useRef<ReturnType<typeof setInterval>>();
 
+  /// Start running the handler every `intervalMs` millis
   const start = useCallback(() => {
     handler();
     interval.current = setInterval(() => {
@@ -11,7 +12,15 @@ const useTimedRefresh = (handler: () => any, intervalMs : number, enabled : bool
     return () => clearInterval(interval.current);
   }, [handler, intervalMs]);
 
-  ///
+  /// Window event handlers
+  const onFocus = useCallback(() => {
+    start();
+  }, [start]);
+  const onBlur = useCallback(() => {
+    clearInterval(interval.current);
+  }, [interval]);
+
+  /// Setup interval on initial load or params change
   useEffect(() => {
     if (interval) clearInterval(interval.current);
     if (enabled) {
@@ -20,14 +29,14 @@ const useTimedRefresh = (handler: () => any, intervalMs : number, enabled : bool
     }
   }, [enabled, interval, start]);
 
-  ///
+  /// Clear interval on blur; re-run on focus
   useEffect(() => {
-    window.addEventListener('focus', () => {
-      start();
-    });
-    window.addEventListener('blur', () => {
-      clearInterval(interval.current);
-    });
+    window.addEventListener('focus', onFocus);
+    window.addEventListener('blur', onBlur);
+    return () => {
+      window.removeEventListener('focus', onFocus);
+      window.removeEventListener('blur', onBlur);
+    };
   });
 };
 
