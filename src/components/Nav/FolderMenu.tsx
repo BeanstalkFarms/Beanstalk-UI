f4dd141bb7edf3f2451052bb9ea195295dc3b19e
Cool Bean <anoncoolbean@gmail.com>
2022-07-29 01:53:39 -0500

    disable event listener on mobile to re-enable scroll on drawer

--- src/components/Nav/FolderMenu.tsx
+++ src/components/Nav/FolderMenu.tsx
@@ -86,85 +86,92 @@ const FolderMenu: React.FC<{
     isOpen ? close() : open();
   }, {}, [isOpen, open, close]);
 
+  const content = (
+    <Box>
+      <Button
+        color="light"
+        startIcon={startIcon}
+        endIcon={<DropdownIcon open={isOpen} />}
+        onClick={open}
+        disableRipple
+        ref={(r) => {
+          button.current = r;
+        }}
+        {...buttonProps}
+        sx={{
+          // Fully rounded by default; when open, remove
+          // the bottom rounding to look like a "tab".
+          borderBottomLeftRadius: popoverOpen ? 0 : undefined,
+          borderBottomRightRadius: popoverOpen ? 0 : undefined,
+          // Enforce a default white border; switch the color
+          // to secondary when the Popper is open.
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: popoverOpen ? 'secondary.main' : 'white',
+          // Keep this white so we can make it look like the
+          // button is "expanding" into a Box when you click it.
+          borderBottomColor: 'white',
+          // Without disabling the transition, the border fades
+          // in/out and looks weird.
+          transition: 'none !important',
+          // Move the button above the Box so we can slice off
+          // the 1px border at the top of the Box.
+          zIndex: popoverOpen ? 999 : undefined,
+          // Positioning and other styles.
+          ...buttonProps.sx,
+        }}
+      >
+        <Box sx={{ display: { xs: hideTextOnMobile ? 'none' : 'block', sm: 'block' } }}>
+          <Typography variant="h3">
+            {buttonContent}
+          </Typography>
+        </Box>
+      </Button>
+      <Popper
+        open={popoverOpen}
+        anchorEl={anchorEl}
+        placement="bottom-start"
+        disablePortal
+      >
+        <Box
+          sx={(_theme) => ({
+            background: 'white',
+            width: popperWidth !== undefined ? popperWidth : '450px',
+            borderBottomLeftRadius: _theme.shape.borderRadius,
+            borderBottomRightRadius: _theme.shape.borderRadius,
+            borderTopRightRadius: _theme.shape.borderRadius,
+            borderColor: 'secondary.main',
+            borderWidth: 1,
+            borderStyle: 'solid',
+            px: 1,
+            py: 1,
+            boxShadow: _theme.shadows[0],
+            // Should be below the zIndex of the Button.
+            zIndex: 998,
+            mt: '-1px',
+          })}
+        >
+          {popoverContent}
+        </Box>
+      </Popper>
+    </Box>
+  );
+  
   return (
     <>
       {/* Mobile: Drawer */}
       <Drawer anchor="bottom" open={drawerOpen} onClose={closeDrawer}>
         {drawerContent}
       </Drawer>
-      <Box>
+      {isMobile ? (
+        <Box>
+          {content}
+        </Box>
+      ) : (
         <ClickAwayListener mouseEvent="onMouseUp" touchEvent="onTouchStart" onClickAway={handleClickAway}>
-          <Box>
-            <Button
-              color="light"
-              startIcon={startIcon}
-              endIcon={<DropdownIcon open={isOpen} />}
-              onClick={open}
-              disableRipple
-              ref={(r) => {
-                button.current = r;
-              }}
-              {...buttonProps}
-              sx={{
-                // Fully rounded by default; when open, remove
-                // the bottom rounding to look like a "tab".
-                borderBottomLeftRadius: popoverOpen ? 0 : undefined,
-                borderBottomRightRadius: popoverOpen ? 0 : undefined,
-                // Enforce a default white border; switch the color
-                // to secondary when the Popper is open.
-                borderWidth: 1,
-                borderStyle: 'solid',
-                borderColor: popoverOpen ? 'secondary.main' : 'white',
-                // Keep this white so we can make it look like the
-                // button is "expanding" into a Box when you click it.
-                borderBottomColor: 'white',
-                // Without disabling the transition, the border fades
-                // in/out and looks weird.
-                transition: 'none !important',
-                // Move the button above the Box so we can slice off
-                // the 1px border at the top of the Box.
-                zIndex: popoverOpen ? 999 : undefined,
-                // Positioning and other styles.
-                ...buttonProps.sx,
-              }}
-            >
-              <Box sx={{ display: { xs: hideTextOnMobile ? 'none' : 'block', sm: 'block' } }}>
-                <Typography variant="h3">
-                  {buttonContent}
-                </Typography>
-              </Box>
-            </Button>
-            <Popper
-              open={popoverOpen}
-              anchorEl={anchorEl}
-              placement="bottom-start"
-              disablePortal
-            >
-              <Box
-                sx={(_theme) => ({
-                  background: 'white',
-                  width: popperWidth !== undefined ? popperWidth : '450px',
-                  borderBottomLeftRadius: _theme.shape.borderRadius,
-                  borderBottomRightRadius: _theme.shape.borderRadius,
-                  borderTopRightRadius: _theme.shape.borderRadius,
-                  borderColor: 'secondary.main',
-                  borderWidth: 1,
-                  borderStyle: 'solid',
-                  px: 1,
-                  py: 1,
-                  boxShadow: _theme.shadows[0],
-                  // Should be below the zIndex of the Button.
-                  zIndex: 998,
-                  mt: '-1px',
-                })}
-              >
-                {popoverContent}
-              </Box>
-            </Popper>
-          </Box>
-
+          {content}
         </ClickAwayListener>
-      </Box>
+      )}
     </>
   );
 };
