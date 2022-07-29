4c9f6d5d1083604a06e3d77519e3b50b83a75f6a
Cool Bean <anoncoolbean@gmail.com>
2022-07-29 01:35:37 -0500

    remove balances warning

--- src/components/Balances/TotalBalancesCard.tsx
+++ src/components/Balances/TotalBalancesCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, AlertTitle, Box, Link, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import useFarmerSiloBreakdown from 'hooks/useFarmerSiloBreakdown';
 import { displayUSD } from 'util/index';
 import Stat from 'components/Common/Stat';
@@ -7,7 +7,6 @@ import { useAccount } from 'wagmi';
 import BlurComponent from 'components/Common/ZeroState/BlurComponent';
 import SiloBalances from 'components/Common/SiloBalances';
 import useChainId from '../../hooks/useChain';
-import { SupportedChainId } from '../../constants';
 import useWhitelist from '../../hooks/useWhitelist';
 import WalletButton from '../Common/Connection/WalletButton';
 
@@ -27,17 +26,6 @@ const TotalBalanceCard: React.FC<TotalBalanceCardProps> = ({ breakdown }) => {
         amountIcon={undefined}
         gap={0}
       />
-      {(account && chainId === SupportedChainId.MAINNET) && (
-        <Alert severity="warning" sx={{ mt: 2, mb: 1 }}>
-          <AlertTitle>Note regarding balances</AlertTitle>
-          Balances are currently fixed to their pre-exploit values. The USD value of Silo Deposits are calculated using a fixed $BEAN
-          price of <strong>$1.02027</strong>.<br />
-          Due to upgrades to the Beanstalk contract and website infrastructure, pre-exploit balances may be temporarily
-          hidden or show incorrect values for some users. Please report issues in <strong>#ui-feedback</strong> and stay
-          tuned for updates in <strong>#ui-updates</strong> on <Link href="https://discord.gg/beanstalk">Discord</Link>. Upgrades will 
-          continue to happen between now and Replant.
-        </Alert>
-      )}
       <Box sx={{ width: '100%', position: 'relative' }}>
         {!account && (
           <BlurComponent>
