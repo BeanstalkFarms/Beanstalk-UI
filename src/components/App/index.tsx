9daedc7ebacc52e05d757e5224c32d56a4f0f136
Silo Chad <silochad@protonmail.com>
2022-08-04 15:24:43 -0500

    hotfix: take website down for Replanting

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BigNumber from 'bignumber.js';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { Box, CssBaseline } from '@mui/material';
+import { Box, Card, CssBaseline, Stack, Typography } from '@mui/material';
 import { ToastBar, Toaster } from 'react-hot-toast';
 import SiloPage from 'pages/silo';
 import SiloTokenPage from 'pages/silo/token';
@@ -31,83 +31,11 @@ import FieldAnalytics from '../../pages/analytics/field';
 import BarnraiseAnalytics from '../../pages/analytics/barnraise';
 import MarketplacePage from '../../pages/marketplace';
 
-BigNumber.set({ EXPONENTIAL_AT: [-12, 20] });
-
-const CustomToaster : React.FC = () => (
-  <Toaster
-    containerStyle={{
-      top: 78,
-    }}
-    toastOptions={{
-      duration: 4000,
-      position: 'top-right',
-      style: {
-        minWidth: 300,
-        maxWidth: 400,
-        paddingLeft: '16px',
-      }
-    }}
-  >
-    {(t) => (
-      <ToastBar
-        toast={t}
-        style={{
-          ...t.style,
-          // Option 1: Pops up instantly,
-          // then slides out to the right side
-          // animation: 'none',
-          // position: 'absolute',
-          // right: t.visible ? 0 : -500,
-          // transition: 'right 0.4s ease-in-out',
-          // opacity: 1,
-          // Option 2: Slides in and out, but there's
-          // an issue where it "flashes back" after
-          // completing the animation.
-          // position: 'absolute',
-          // animation: t.visible ? 'custom-enter 1s ease-in-out' : 'custom-exit 1s ease-in-out',
-          // animationFillMode: 'forwards'
-          // Option 3: Tries to fix toasts stacking on top of each other
-          animation: 'none',
-          marginRight: t.visible ? 0 : -500,
-          transition: 'margin-right 0.4s ease-in-out',
-          opacity: 1,
-        }}
-      />
-    )}
-  </Toaster>
-);
-
 export default function App() {
   const location = useLocation();
   return (
     <>
       <CssBaseline />
-      {/* -----------------------
-        * Bean Updaters
-        * ----------------------- */}
-      <PoolsUpdater />
-      {/* -----------------------
-        * Beanstalk Updaters
-        * ----------------------- */}
-      <FertilizerUpdater />
-      <FieldUpdater />
-      <SiloUpdater />
-      <SunUpdater />
-      {/* -----------------------
-        * Farmer Updaters
-        * ----------------------- */}
-      <FarmerEventsUpdater />
-      <FarmerEventsProcessor />
-      <FarmerBalancesUpdater />
-      <FarmerFertilizerUpdater />
-      <FarmerSiloUpdater />
-      {/* -----------------------
-        * Content
-        * ----------------------- */}
-      <NavBar />
-      <CustomToaster />
-      {/* only show welcome back modal on non barn-raise pages */}
-      {location.pathname !== '/' && <WelcomeBackModal />}
       <Box
         sx={{
           backgroundColor: BeanstalkPalette.lighterBlue,
@@ -118,29 +46,13 @@ export default function App() {
           backgroundRepeat: 'no-repeat',
           width: '100%',
           minHeight: '100vh',
-          paddingTop: {
-            md: 4,
-            xs: 2,
-          },
-          paddingBottom: {
-            md: 4,
-            xs: 2 
-          }
         }}>
-        <Routes>
-          <Route path="/" element={<BarnRaisePage />} />
-          <Route path="/silo" element={<SiloPage />} />
-          <Route path="/silo/:address" element={<SiloTokenPage />} />
-          <Route path="/field" element={<FieldPage />} />
-          <Route path="/forecast" element={<ForecastPage />} />
-          <Route path="/history" element={<TransactionHistoryPage />} />
-          <Route path="/balances" element={<BalancesPage />} />
-          <Route path="/analytics/bean" element={<BeanAnalytics />} />
-          <Route path="/analytics/silo" element={<SiloAnalytics />} />
-          <Route path="/analytics/field" element={<FieldAnalytics />} />
-          <Route path="/analytics/barnraise" element={<BarnraiseAnalytics />} />
-          <Route path="/market" element={<MarketplacePage />} />
-        </Routes>
+        <Stack direction="row" height="100vh" width="100%" alignItems="center" justifyContent="center">
+          <Card sx={{ p: 2 }}>
+            <Typography variant="h2" mb={1}>Beanstalk is currently Replanting.</Typography>
+            <Typography>The new Beanstalk UI will be launched upon completion of the Replant. See Discord for more information.</Typography>
+          </Card>
+        </Stack>
       </Box>
     </>
   );
