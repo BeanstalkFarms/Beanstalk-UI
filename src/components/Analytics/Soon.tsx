e03de609bcd07b8ef490312ec048dcc56c28cb45
Silo Chad <silochad@protonmail.com>
2022-08-04 17:56:17 -0500

    add curve factory link to trade

--- src/components/Analytics/Soon.tsx
+++ src/components/Analytics/Soon.tsx
@@ -1,10 +1,10 @@
 import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
-const Soon : React.FC<{ height?: number }> = ({ height = 300 }) => (
+const Soon : React.FC<{ height?: number }> = ({ height = 300, children }) => (
   <Stack direction="row" alignItems="center" justifyContent="center" sx={{ width: '100%', height }} p={2}>
     <Typography textAlign="center" color="gray">
-      This module is under development and will be available soon.
+      {children || 'This module is under development and will be available soon.'}
     </Typography>
   </Stack>
 );
--- src/components/Trade/Actions/index.tsx
+++ src/components/Trade/Actions/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Card, Stack, Typography } from '@mui/material';
+import { Box, Card, Link, Stack, Typography } from '@mui/material';
+import Soon from 'components/Analytics/Soon';
 import React from 'react';
-import Trade from './Trade';
 
 const TradeActions : React.FC<{}> = () => (
   <Card sx={{ position: 'relative' }}>
@@ -8,8 +8,11 @@ const TradeActions : React.FC<{}> = () => (
       <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ overflow: 'visible', px: 2, pt: 2 }}>
         <Typography variant="h4">Trade</Typography>
       </Stack>
-      <Box sx={{ px: 1, pb: 1 }}>
-        <Trade />
+      <Box sx={{ px: 1, pb: 1, position: 'relative' }}>
+        {/* <Trade /> */}
+        <Soon>
+          This module is under development. Trade Beans directly via the Curve website: <strong><Link href="https://curve.fi/factory/152" target="_blank" rel="noreferrer">https://curve.fi/factory/152</Link></strong>
+        </Soon>
       </Box>
     </Stack>
   </Card>
