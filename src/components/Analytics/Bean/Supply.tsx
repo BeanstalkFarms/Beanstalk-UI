20458a07f951a09ce35db8992f2cf5fc29adf124
cujowolf <cujowolf12@gmail.com>
2022-08-04 22:03:53 -0500

    Update supply graph

--- src/components/Analytics/Bean/Supply.tsx
+++ src/components/Analytics/Bean/Supply.tsx
@@ -9,7 +9,7 @@ import { useTheme } from '@mui/material/styles';
 import { BEAN } from '../../../constants/tokens';
 import { toTokenUnitsBN } from '../../../util';
 
-const getValue = (season: SnapshotData<SeasonalSupplyQuery>) => toTokenUnitsBN(season.totalPods, BEAN[1].decimals).toNumber();
+const getValue = (season: SnapshotData<SeasonalSupplyQuery>) => toTokenUnitsBN(season.beans, BEAN[1].decimals).toNumber();
 const formatValue = (value: number) => `${value.toLocaleString('en-US', { maximumFractionDigits: 0 })}`;
 const useStatProps = () => {
     const theme = useTheme();
