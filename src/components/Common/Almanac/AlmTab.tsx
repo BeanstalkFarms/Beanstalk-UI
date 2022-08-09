ff0e3155a1ca4ea26bb6dde50fc1d31ead9e3919
Silo Chad <silochad@protonmail.com>
2022-08-09 18:49:49 -0500

    fix: remove AlmTab

--- src/components/Common/Almanac/AlmTab.tsx
+++ src/components/Common/Almanac/AlmTab.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Tab, TabProps } from '@mui/material';
-import useAppFlag from '~/hooks/display/useAppFlag';
+// import React from 'react';
+// import { Tab, TabProps } from '@mui/material';
+// import useAppFlag from '~/hooks/display/useAppFlag';
 
-const open = (url: string) => (e: any) => {
-  e.preventDefault();
-  e.stopPropagation();
-  window.open(url, `_newtab${new Date().getTime()}`);
-};
+// const open = (url: string) => (e: any) => {
+//   e.preventDefault();
+//   e.stopPropagation();
+//   window.open(url, `_newtab${new Date().getTime()}`);
+// };
 
-const prefix = (uri: string) => (uri ? uri.charAt(0) === '/' ? `https://beanstalk.gitbook.io/wip-farmers-almanac${uri}` : uri : '');
+// const prefix = (uri: string) => (uri ? uri.charAt(0) === '/' ? `https://beanstalk.gitbook.io/wip-farmers-almanac${uri}` : uri : '');
 
-const almProps = (props: any, pressed = false) => ({
-    onClick: pressed ? open(prefix(props.almHref)) : props.onClick,
-    className: pressed ? `alm ${props.className}` : props.className,
-  });
+// const almProps = (props: any, pressed = false) => ({
+//     onClick: pressed ? open(prefix(props.almHref)) : props.onClick,
+//     className: pressed ? `alm ${props.className}` : props.className,
+//   });
 
-type AlmProps = {
-  almHref: string;
-}
+// type AlmProps = {
+//   almHref: string;
+// }
 
-const AlmTab : React.FC<TabProps & AlmProps> = ({ ...props }) => {
-  const pressed = useAppFlag('almanacView');
-  return (
-    <Tab
-      {...props}
-      {...almProps(props, pressed as boolean)}
-    />
-  );
-};
+// const AlmTab : React.FC<TabProps & AlmProps> = ({ ...props }) => {
+//   const pressed = useAppFlag('almanacView');
+//   return (
+//     <Tab
+//       {...props}
+//       {...almProps(props, pressed as boolean)}
+//     />
+//   );
+// };
 
-export default AlmTab;
+export default null;
