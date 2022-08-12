f737915893a18bd0ce25ba68513d1dbcdebc7697
Silo Chad <silochad@protonmail.com>
2022-08-12 10:47:18 -0500

    add: snapshot links as banner

--- src/pages/silo/index.tsx
+++ src/pages/silo/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Box, Button, Card, Container, Stack, Typography, Alert, Link } from '@mui/material';
+import { Box, Button, Card, Container, Stack, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Overview from '~/components/Silo/Overview';
 import RewardsBar from '~/components/Silo/RewardsBar';
@@ -65,14 +65,6 @@ const SiloPage : React.FC = () => {
           breakdown={breakdown}
           season={season}
         />
-        <Alert severity="info" variant="standard" sx={{ borderColor: 'secondary.dark', borderWidth: 1, borderStyle: 'solid' }}>
-          <Box sx={{ ml: 1 }}>
-            <Link href="https://snapshot.org/#/beanstalkdao.eth/proposal/0x770efe960a45d7f91b21b6b13106412b666ad5f90c2a27e398867972aa16f893" target="_blank" rel="noreferrer" underline="hover">BIP-22</Link> and&nbsp;
-            <Link href="https://snapshot.org/#/beanstalkdao.eth/proposal/0x3b2a7808f01960ff993b7aec4df9ef6a3434d0ef0843828ea6c6bce4e768e6a9" target="_blank" rel="noreferrer" underline="hover">BIP-23</Link>,
-            the Q3 budget proposals for Beanstalk Farms and Bean Sprout respectively, are live for voting on&nbsp;
-            <Link href="https://snapshot.org/#/beanstalkdao.eth/" target="_blank" rel="noreferrer" underline="hover">Snapshot</Link>.
-          </Box>
-        </Alert>
         <Card>
           <Box sx={{ width: '100%', height: '100%', position: 'relative' }}>
             {(!account || breakdown.totalValue?.eq(0)) && (
