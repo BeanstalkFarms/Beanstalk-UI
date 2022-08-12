469bf5c2c38d6d5cf53c0965a932f767cfff06f0
Cool Bean <anoncoolbean@gmail.com>
2022-08-12 12:42:59 -0500

    small mobile tweaks

--- src/pages/barn.tsx
+++ src/pages/barn.tsx
@@ -5,14 +5,14 @@ import RemainingFertilizer from '~/components/Barn/RemainingFertilizer';
 import MyFertilizer from '~/components/Barn/MyFertilizer';
 import BarnActions from '~/components/Barn/Actions';
 import GuideButton from '~/components/Common/Guide/GuideButton';
-import { HOW_TO_BUY_FERTILIZER, HOW_TO_RINSE_SPROUTS } from '~/util/Guides';
+ import { HOW_TO_BUY_FERTILIZER, HOW_TO_RINSE_SPROUTS } from '~/util/Guides';
 
 const Barn: React.FC = () => (
   <Container maxWidth="sm">
     <Stack gap={2}>
       <PageHeader
         title="The Barn"
-        description="Earn yield and recapitalize Beanstalk by buying Fertilizer"
+        description="Earn yield and recapitalize Beanstalk with Fertilizer"
         href="https://docs.bean.money/farm/barn"
         OuterStackProps={{ direction: 'row' }}
         control={
