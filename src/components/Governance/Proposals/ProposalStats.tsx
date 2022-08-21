11d4e48e28f8311fd60c8c64c7c626463e9f991d
Cool Bean <anoncoolbean@gmail.com>
2022-08-21 16:30:00 -0500

    NaN% voted for when numerator was undefined

--- src/components/Governance/Proposals/ProposalStats.tsx
+++ src/components/Governance/Proposals/ProposalStats.tsx
@@ -4,6 +4,7 @@ import BigNumber from 'bignumber.js';
 import { getDateMessage, Proposal } from '~/util/Governance';
 import { displayBN } from '~/util';
 import Dot from '~/components/Common/Dot';
+import { ZERO_BN } from '~/constants';
 
 const ProposalStats: React.FC<{
   proposal: Proposal;
@@ -28,31 +29,31 @@ const ProposalStats: React.FC<{
         {getDateMessage(proposal.end)}
       </Typography>
       {showLink && (
-        <Link
-          href={`https://snapshot.org/#/${proposal.space}/proposal/${proposal.id}`}
-          target="_blank"
-          rel="noreferrer"
-          underline="hover"
-            >
-          View on Snapshot
-        </Link>
-      )}
+      <Link
+        href={`https://snapshot.org/#/${proposal.space}/proposal/${proposal.id}`}
+        target="_blank"
+        rel="noreferrer"
+        underline="hover"
+          >
+        View on Snapshot
+      </Link>
+        )}
     </Stack>
     {/* if there is time remaining... */}
     {(differenceInTime && differenceInTime > 0 && totalStalk) && (
-      <Stack direction="row" alignItems="center" gap={0.5} display={{ xs: 'none', md: 'block' }}>
-        {proposal.space.id === 'wearebeansprout.eth' ? (
-          <Typography textAlign={{ xs: 'center', md: 'left' }} variant="body1">
-            {displayBN(new BigNumber(proposal.scores_total).div(totalStalk).multipliedBy(100))}% of Stalk voted
-          </Typography>
-        ) : (
-          <Typography textAlign={{ xs: 'center', md: 'left' }} variant="body1">
-            {displayBN(new BigNumber(proposal.scores[0]).div(totalStalk).multipliedBy(100))}% of Stalk voted For
-          </Typography>
-        )}
-      </Stack>
-    )}  
+    <Stack direction="row" alignItems="center" gap={0.5} display={{ xs: 'none', md: 'block' }}>
+      {proposal.space.id === 'wearebeansprout.eth' ? (
+        <Typography textAlign={{ xs: 'center', md: 'left' }} variant="body1">
+          {displayBN(new BigNumber(proposal.scores_total ? proposal.scores_total : ZERO_BN).div(totalStalk).multipliedBy(100))}% of Stalk voted
+        </Typography>
+          ) : (
+            <Typography textAlign={{ xs: 'center', md: 'left' }} variant="body1">
+              {displayBN(new BigNumber(proposal.scores[0] ? proposal.scores[0] : ZERO_BN).div(totalStalk).multipliedBy(100))}% of Stalk voted For
+            </Typography>
+          )}
+    </Stack>
+      )}
   </Stack>
-);
+  );
 
 export default ProposalStats;
