0da5b02a1991dae2695cfa35ac4724dec99fd9a4
Cool Bean <anoncoolbean@gmail.com>
2022-08-22 14:20:48 -0500

    add 404 page & fix website breaking on invalid governance id page

---
+++ src/pages/error/404.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Card,
+  Container,
+  Stack, Typography
+} from '@mui/material';
+import forecast from '~/img/beanstalk/forecast-banner.svg';
+
+const PageNotFound: React.FC = () => (
+  <Container maxWidth="md">
+    <Card sx={{ px: 4, py: 6 }}>
+      <Stack direction="column" alignItems="center" justifyContent="center" gap={1}>
+        <img src={forecast} alt="Barn" style={{ maxWidth: 400, marginBottom: 20 }} />
+        <Typography variant="h1">404 error</Typography>
+        <Typography color="text.secondary" variant="h3">The page you are looking for does not exist!</Typography>
+      </Stack>
+    </Card>
+  </Container>
+);
+
+export default PageNotFound;
--- src/pages/governance/proposal.tsx
+++ src/pages/governance/proposal.tsx
@@ -3,24 +3,33 @@ import {
   Container, Grid,
   Stack
 } from '@mui/material';
+import { useParams } from 'react-router-dom';
 import PageHeader from '~/components/Common/PageHeader';
 import GovernanceActions from '~/components/Governance/Actions';
 import ProposalContent from '~/components/Governance/Proposal';
 
-const ProposalPage: React.FC = () => (
-  <Container maxWidth="lg">
-    <Stack gap={2}>
-      <PageHeader returnPath="/governance" />
-      <Grid container direction={{ xs: 'column-reverse', md: 'row' }} spacing={2}>
-        <Grid item xs={12} md={8}>
-          <ProposalContent />
+const ProposalPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+
+  if (!id) {
+    return null;
+  }
+  
+  return (
+    <Container maxWidth="lg">
+      <Stack gap={2}>
+        <PageHeader returnPath="/governance" />
+        <Grid container direction={{ xs: 'column-reverse', md: 'row' }} spacing={2}>
+          <Grid item xs={12} md={8}>
+            <ProposalContent />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <GovernanceActions />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <GovernanceActions />
-        </Grid>
-      </Grid>
-    </Stack>
-  </Container>
-);
+      </Stack>
+    </Container>
+  );
+};
 
 export default ProposalPage;
