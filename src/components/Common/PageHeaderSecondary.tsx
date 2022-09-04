4aa41fdbf4290357b6a6a4d3090f51530cd02802
Cool Bean <anoncoolbean@gmail.com>
2022-09-03 22:49:34 -0500

    refactor secondaryHeader & add header buttons

--- src/components/Common/PageHeaderSecondary.tsx
+++ src/components/Common/PageHeaderSecondary.tsx
@@ -1,10 +1,11 @@
-import { useNavigate , Link as RouterLink } from 'react-router-dom';
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
 import { Box, Button, Stack, Typography } from '@mui/material';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import React from 'react';
 import { BeanstalkPalette, IconSize } from '../App/muiTheme';
+import Row from '~/components/Common/Row';
 
-const PageHeader : React.FC<{
+const PageHeader: React.FC<{
   /** The Field: The Decentralized Credit Facility */
   title?: string | JSX.Element;
   icon?: JSX.Element;
@@ -12,6 +13,7 @@ const PageHeader : React.FC<{
   returnPath?: string;
   /**  */
   control?: React.ReactElement;
+  alignTitle?: 'left' | 'center';
 }> = (props) => {
   const navigate = useNavigate();
   const buttonProps = props.returnPath ? {
@@ -20,31 +22,32 @@ const PageHeader : React.FC<{
   } : {
     onClick: () => navigate(-1),
   };
-  return (
-    <Box>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" gap={0.5}>
-        <Stack sx={{ width: 70, justifyContent: 'start' }}>
-          <Button
-            {...buttonProps}
-            color="naked"
-            sx={{
-              p: 0,
-              borderRadius: 1,
-              float: 'left',
-              display: 'inline',
-              mb: '-2.5px',
-              '&:hover': {
-                color: BeanstalkPalette.logoGreen,
-              }
-            }}
-          >
-            <Stack direction="row" gap={0.5} alignItems="center" height="100%">
-              <KeyboardBackspaceIcon sx={{ width: IconSize.small }} height="auto" />
-              <Typography variant="h4">Back</Typography>
-            </Stack>
-          </Button>
-        </Stack>
-        {props.title && (
+
+  const mainContent = (
+    <>
+      <Stack sx={{ width: 70, justifyContent: 'start' }}>
+        <Button
+          {...buttonProps}
+          color="naked"
+          sx={{
+            p: 0,
+            borderRadius: 1,
+            float: 'left',
+            display: 'inline',
+            mb: '-2.5px',
+            '&:hover': {
+              color: BeanstalkPalette.logoGreen,
+            }
+          }}
+        >
+          <Stack direction="row" gap={0.5} alignItems="center" height="100%">
+            <KeyboardBackspaceIcon sx={{ width: IconSize.small }} height="auto" />
+            <Typography variant="h4">Back</Typography>
+          </Stack>
+        </Button>
+      </Stack>
+      {
+        props.title && (
           <>
             {typeof props.title === 'string' ? (
               <Typography
@@ -61,8 +64,22 @@ const PageHeader : React.FC<{
               </>
             )}
           </>
+        )
+      }
+    </>
+
+  );
+  return (
+    <Box>
+      <Stack direction={{ xs: 'column', sm: 'row' }} alignItems={{ xs: 'start', sm: 'center' }} justifyContent={{ xs: 'start', sm: 'space-between' }} gap={0.5}>
+        {props.alignTitle === 'left' ? (
+          <Row>
+            {mainContent}
+          </Row>
+        ) : (
+          <>{mainContent}</>
         )}
-        <Box sx={{ width: 70 }} display="flex" justifyContent="end">
+        <Box sx={{ width: props.alignTitle === 'left' ? 'fit-content' : 70 }} display="flex" justifyContent="end">
           {props.control ? (props.control) : null}
         </Box>
       </Stack>
