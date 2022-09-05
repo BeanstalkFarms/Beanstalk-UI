f1e019e2a343d1d7338d85d18f5bb58b3907b9c2
Silo Chad <silochad@protonmail.com>
2022-09-05 17:05:07 -0500

    hotfix: remove chop

--- src/components/Chop/Actions/index.tsx
+++ src/components/Chop/Actions/index.tsx
@@ -1,6 +1,7 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 import { Module, ModuleContent, ModuleHeader } from '~/components/Common/Module';
+import BlurComponent from '~/components/Common/ZeroState/BlurComponent';
 import Chop from './Chop';
 
 const ChopActions : React.FC<{}> = () => (
@@ -9,6 +10,9 @@ const ChopActions : React.FC<{}> = () => (
       <Typography variant="h4">Chop</Typography>
     </ModuleHeader>
     <ModuleContent>
+      <BlurComponent>
+        Chopping is temporarily disabled. Check Discord for more information.
+      </BlurComponent>
       <Chop />
     </ModuleContent>
   </Module>
