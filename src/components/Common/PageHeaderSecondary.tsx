63700fcdeba03db56baafa92ab8450ff7ce19cc9
Bean-Sama <anoncham65@gmail.com>
2022-09-08 18:03:22 -0400

    header

--- src/components/Common/PageHeaderSecondary.tsx
+++ src/components/Common/PageHeaderSecondary.tsx
@@ -1,12 +1,13 @@
-import { useNavigate , Link as RouterLink } from 'react-router-dom';
-import { Box, Button, Stack, Typography } from '@mui/material';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import React from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { BeanstalkPalette, IconSize } from '../App/muiTheme';
 
-const PageHeader : React.FC<{
+const PageHeader: React.FC<{
   /** The Field: The Decentralized Credit Facility */
   title?: string | JSX.Element;
+  titleAlign?: 'left' | 'center';
   icon?: JSX.Element;
   /** Show a back button to return to `returnPath`. */
   returnPath?: string;
@@ -14,15 +15,23 @@ const PageHeader : React.FC<{
   control?: React.ReactElement;
 }> = (props) => {
   const navigate = useNavigate();
-  const buttonProps = props.returnPath ? {
-    to: props.returnPath,
-    component: RouterLink,
-  } : {
-    onClick: () => navigate(-1),
-  };
+  const buttonProps = props.returnPath
+    ? {
+        to: props.returnPath,
+        component: RouterLink,
+      }
+    : {
+        onClick: () => navigate(-1),
+      };
+
   return (
     <Box>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" gap={0.5}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        gap={0.5}
+      >
         <Stack sx={{ width: 70, justifyContent: 'start' }}>
           <Button
             {...buttonProps}
@@ -35,11 +44,14 @@ const PageHeader : React.FC<{
               mb: '-2.5px',
               '&:hover': {
                 color: BeanstalkPalette.logoGreen,
-              }
+              },
             }}
           >
             <Stack direction="row" gap={0.5} alignItems="center" height="100%">
-              <KeyboardBackspaceIcon sx={{ width: IconSize.small }} height="auto" />
+              <KeyboardBackspaceIcon
+                sx={{ width: IconSize.small }}
+                height="auto"
+              />
               <Typography variant="h4">Back</Typography>
             </Stack>
           </Button>
@@ -49,21 +61,23 @@ const PageHeader : React.FC<{
             {typeof props.title === 'string' ? (
               <Typography
                 variant="h2"
-                textAlign="center"
-                sx={{ verticalAlign: 'middle' }}
+                textAlign={props.titleAlign ?? 'center'}
+                sx={{
+                  ml: props.titleAlign ? 1.5 : 0,
+                  verticalAlign: 'middle',
+                  width: '100%',
+                }}
               >
                 {props.icon}&nbsp;
                 {props.title}
               </Typography>
             ) : (
-              <>
-                {props.title}
-              </>
+              props.title
             )}
           </>
         )}
         <Box sx={{ width: 70 }} display="flex" justifyContent="end">
-          {props.control ? (props.control) : null}
+          {props.control ? props.control : null}
         </Box>
       </Stack>
     </Box>
--- src/pages/silo/token.tsx
+++ src/pages/silo/token.tsx
@@ -1,40 +1,44 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { Container, Stack } from '@mui/material';
-import SiloActions from '~/components/Silo/Actions';
-import PoolCard from '~/components/Silo/PoolCard';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { ERC20Token } from '~/classes/Token';
+import GuideButton from '~/components/Common/Guide/GuideButton';
 import PageHeaderSecondary from '~/components/Common/PageHeaderSecondary';
 import TokenIcon from '~/components/Common/TokenIcon';
-import { ERC20Token } from '~/classes/Token';
+import SiloActions from '~/components/Silo/Actions';
+import PoolCard from '~/components/Silo/PoolCard';
 import usePools from '~/hooks/beanstalk/usePools';
 import useWhitelist from '~/hooks/beanstalk/useWhitelist';
 import { AppState } from '~/state';
-import GuideButton from '~/components/Common/Guide/GuideButton';
-import { HOW_TO_CLAIM_WITHDRAWALS,
+import {
+  HOW_TO_CLAIM_WITHDRAWALS,
   HOW_TO_CONVERT_DEPOSITS,
-  HOW_TO_DEPOSIT_IN_THE_SILO, HOW_TO_TRANSFER_DEPOSITS,
-  HOW_TO_WITHDRAW_FROM_THE_SILO
+  HOW_TO_DEPOSIT_IN_THE_SILO,
+  HOW_TO_TRANSFER_DEPOSITS,
+  HOW_TO_WITHDRAW_FROM_THE_SILO,
 } from '~/util/Guides';
 
 const TokenPage: React.FC<{}> = () => {
   // Constants
   const whitelist = useWhitelist();
-  const pools     = usePools();
+  const pools = usePools();
 
   // Routing
   let { address } = useParams<{ address: string }>();
   address = address?.toLowerCase();
 
   // State
-  const farmerSilo = useSelector<AppState, AppState['_farmer']['silo']>((state) => state._farmer.silo);
-  const poolStates = useSelector<AppState, AppState['_bean']['pools']>((state) =>  state._bean.pools);
+  const farmerSilo = useSelector<AppState, AppState['_farmer']['silo']>(
+    (state) => state._farmer.silo
+  );
+  const poolStates = useSelector<AppState, AppState['_bean']['pools']>(
+    (state) => state._bean.pools
+  );
 
   // Ensure this address is a whitelisted token
   if (!address || !whitelist?.[address]) {
-    return (
-      <div>Not found</div>
-    );
+    return <div>Not found</div>;
   }
 
   // Load this Token from the whitelist
@@ -43,18 +47,24 @@ const TokenPage: React.FC<{}> = () => {
 
   // Most Silo Tokens will have a corresponding Pool.
   // If one is available, show a PoolCard with state info.
-  const pool      = pools[address];
+  const pool = pools[address];
   const poolState = poolStates[address];
-  
+
   // If no data loaded...
   if (!whitelistedToken) return null;
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="lg">
       <Stack gap={2}>
         <PageHeaderSecondary
           title={whitelistedToken.name}
-          icon={<TokenIcon style={{ marginTop: 3 }} token={whitelistedToken} />}
+          icon={
+            <TokenIcon
+              style={{ marginTop: 3, alignSelf: 'flex-start' }}
+              token={whitelistedToken}
+            />
+          }
+          titleAlign="left"
           returnPath="/silo"
           control={
             <GuideButton
@@ -73,11 +83,11 @@ const TokenPage: React.FC<{}> = () => {
           <PoolCard
             pool={pool}
             poolState={poolState}
-          //   ButtonProps={{
-          //     href: `https://etherscan.io/address/${pool.address}`,
-          //     target: '_blank',
-          //     rel: 'noreferrer'
-          //   }}
+            //   ButtonProps={{
+            //     href: `https://etherscan.io/address/${pool.address}`,
+            //     target: '_blank',
+            //     rel: 'noreferrer'
+            //   }}
           />
         )}
         <SiloActions
