befde0ef53fc89d399d8a960bb9335bbda9f71e9
Silo Chad <silochad@protonmail.com>
2022-09-13 15:03:55 -0500

    refactor: use Link instead of styled Typography for PagePath

--- src/components/Common/PagePath.tsx
+++ src/components/Common/PagePath.tsx
@@ -1,7 +1,6 @@
-import { Typography, TypographyProps } from '@mui/material';
+import { Link, Typography, TypographyProps } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Row from '~/components/Common/Row';
+import { Link as RouterLink } from 'react-router-dom';
 import { BeanstalkPalette } from '~/components/App/muiTheme';
 
 export type PathItem = {
@@ -9,38 +8,55 @@ export type PathItem = {
   path: string;
 }
 
+const StyledTypography: React.FC<TypographyProps & { isLastItem?: boolean; }> = ({ children, isLastItem }) => (
+  <Typography
+    variant="h4"
+    color={isLastItem ? BeanstalkPalette.black : BeanstalkPalette.grey}
+    sx={{
+      display: 'inline',
+      mr: 1,
+      '&:hover': {
+        color: BeanstalkPalette.black
+      }
+    }}
+  >
+    {children}
+  </Typography>
+);
+
 const PagePath: React.FC<{
   items: PathItem[];
 }> = (props) => (
-  <Row gap={1}>
+  <div>
     {props.items.map((item, index) => {
-        const isLastItem = props.items.length - 1 === index;
-        const StyledTypography: React.FC<TypographyProps> = ({ children }) => (
-          <Typography
+      const isLastItem = props.items.length - 1 === index;
+      return (
+        <span key={index}>
+          <Link
+            component={RouterLink}
+            to={item.path}
+            sx={{ color: isLastItem ? 'text.primary' : 'text.secondary', '&:hover': { color: 'text.primary' } }}
+            display="inline"
+            underline="none"
             variant="h4"
-            color={isLastItem ? BeanstalkPalette.black : BeanstalkPalette.grey}
-            sx={{
-              '&:hover': {
-                color: BeanstalkPalette.black
-              }
-            }}
+            mr={1}
           >
-            {children}
-          </Typography>
-        );
-        return (
-          <>
-            <Link to={item.path} style={{ textDecoration: 'none' }}>
-              <StyledTypography>{item.title}</StyledTypography>
-            </Link>
-            {!isLastItem && (
-              <StyledTypography>{'>'}</StyledTypography>
-            )}
-          </>
-        );
-      }
-    )}
-  </Row>
+            {item.title}
+          </Link>
+          {isLastItem ? null : (
+            <Typography
+              color="text.secondary"
+              display="inline"
+              variant="h4"
+              mr={1}
+            >
+              {'>'}  
+            </Typography>
+          )}
+        </span>
+      );
+    })}
+  </div>
 );
 
 export default PagePath;
