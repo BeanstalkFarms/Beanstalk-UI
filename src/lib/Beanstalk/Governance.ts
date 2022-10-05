d88af2073659dace636648aacf5494e6069b9094
Silo Chad <silochad@protonmail.com>
2022-10-05 03:02:18 -0500

    chore: add other proposal types

--- src/lib/Beanstalk/Governance.ts
+++ src/lib/Beanstalk/Governance.ts
@@ -7,20 +7,25 @@ export enum GovSpace {
 }
 
 export enum GovProposalType {
+  BIP = 'BIP',
+  BOP = 'BOP',
   BFCP_A = 'BFCP-A',
   BFCP_B = 'BFCP-B',
   BFCP_C = 'BFCP-C',
   BFCP_D = 'BFCP-D',
-  BIP = 'BIP',
-  BOP = 'BOP',
+  BSP = 'BSP',
 }
 
 export const SNAPSHOT_SPACES = Object.values(GovSpace);
 
 const QUORUM = {
   [GovProposalType.BIP]:     0.5,
+  [GovProposalType.BOP]:     0.35,
   [GovProposalType.BFCP_A]:  0.25,
+  [GovProposalType.BFCP_B]:  0.25,
   [GovProposalType.BFCP_C]:  0.25,
+  [GovProposalType.BFCP_D]:  0.25,
+  [GovProposalType.BSP]:     0.1,
 };
 
 export const getQuorumPct = (type: string) => {
