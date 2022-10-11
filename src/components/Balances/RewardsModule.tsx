637a0715fe8338e5a70eaf4a12f7dee6285286fe
Cool Bean <anoncoolbean@gmail.com>
2022-10-11 13:47:40 -0500

    wip: tweak component structure

---
+++ src/components/Balances/RewardsModule.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+import { Module, ModuleContent, ModuleHeader } from '~/components/Common/Module';
+
+const RewardsModule : React.FC<{}> = ({
+  children,
+  ...props
+}) => (
+  <Module sx={{ height: '100%' }}>
+    <ModuleHeader>
+      <Typography variant="h4">Rewards</Typography>
+    </ModuleHeader>
+    <ModuleContent px={2} pb={2}>
+      <Typography>Rewards here</Typography>
+    </ModuleContent>
+  </Module>
+);
+
+export default RewardsModule;
--- src/components/Balances/UserBalancesCharts.tsx
+++ src/components/Balances/UserBalancesCharts.tsx
@@ -1,13 +1,22 @@
-import { Card, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import React from 'react';
+import { Module, ModuleContent, ModuleHeader } from '~/components/Common/Module';
+import EmbeddedCard from '~/components/Common/EmbeddedCard';
 
 const UserBalancesCharts : React.FC<{}> = ({
   children,
   ...props
 }) => (
-  <Card sx={{ p: 2, height: '100%' }}>
-    <Typography variant="h4">Deposited Balance</Typography>
-  </Card>
+  <Module>
+    <ModuleHeader>
+      <Typography variant="h4">Deposited Balance</Typography>
+    </ModuleHeader>
+    <ModuleContent px={2} pb={2}>
+      <EmbeddedCard sx={{ pt: 2 }}>
+        <Typography>Charts Here</Typography>
+      </EmbeddedCard>
+    </ModuleContent>
+  </Module>
 );
 
 export default UserBalancesCharts;
