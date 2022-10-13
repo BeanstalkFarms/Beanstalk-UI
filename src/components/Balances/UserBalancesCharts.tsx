337e98c7ee14bcc95a3beadc8520819cdfac2450
Cool Bean <anoncoolbean@gmail.com>
2022-10-13 14:18:13 -0500

    wip: reset StackedAreaChart with bean-samas

--- src/components/Balances/UserBalancesCharts.tsx
+++ src/components/Balances/UserBalancesCharts.tsx
@@ -1,15 +1,15 @@
 import { Typography } from '@mui/material';
-import React, { useMemo } from 'react';
+import React from 'react';
 import BigNumber from 'bignumber.js';
 import { Module, ModuleContent, ModuleHeader } from '~/components/Common/Module';
-import BalancesOverTime from '~/components/Balances/BalancesOverTime';
 import useAccount from '~/hooks/ledger/useAccount';
 import useFarmerBalancesBreakdown from '~/hooks/farmer/useFarmerBalancesBreakdown';
 import useSeason from '~/hooks/beanstalk/useSeason';
 import Stat from '~/components/Common/Stat';
 import { displayUSD } from '~/util';
 import useFarmerBalancesOverview from '~/hooks/farmer/useFarmerBalancesOverview';
-import { DataPoint2 } from '~/components/Common/Charts/StackedAreaChart2';
+import { BaseDataPoint } from '~/components/Common/Charts/ChartPropProvider';
+import useTimeTabState from '~/hooks/app/useTimeTabState';
 
 const depositStats = (s: BigNumber, v: BigNumber[]) => (
   <Stat
@@ -34,9 +34,19 @@ const depositStats = (s: BigNumber, v: BigNumber[]) => (
 const UserBalancesCharts : React.FC<{}> = () => {
   //
   const account = useAccount();
+  const timeTabParams = useTimeTabState();
   const { data, loading } = useFarmerBalancesOverview(account);
   const breakdown     = useFarmerBalancesBreakdown();
   const season        = useSeason();
+
+  const formatValue = (value: number) =>
+  `${value.toLocaleString('en-US', { maximumFractionDigits: 2 })}`;
+
+  const getStatValue = <T extends BaseDataPoint>(v?: T[]) => {
+    if (!v?.length) return 0;
+    const dataPoint = v[0];
+    return dataPoint?.value || 0;
+  };
   
   return (
     <Module>
@@ -44,20 +54,35 @@ const UserBalancesCharts : React.FC<{}> = () => {
         <Typography variant="h4">Deposited Balance</Typography>
       </ModuleHeader>
       <ModuleContent px={2} pb={2}>
-        <BalancesOverTime
-          label="Silo Deposits"
-          account={account}
-          current={useMemo(() => ([
-            breakdown.states.deposited.value
-          ]), [breakdown.states.deposited.value])}
-          series={useMemo(() => ([
-            data.deposits
-          ]), [data.deposits]) as DataPoint2[][]}
-          season={season}
-          stats={depositStats}
-          loading={loading}
-          empty={breakdown.states.deposited.value.eq(0)}
-        />
+        {/* <BaseSeasonPlot */}
+        {/*  queryData={queryParams} */}
+        {/*  height={300} */}
+        {/*  StatProps={{ */}
+        {/*    title: 'Total Deposited Bean & Bean3Crv', */}
+        {/*    gap: 0.5, */}
+        {/*  }} */}
+        {/*  timeTabParams={timeTabParams} */}
+        {/*  formatValue={formatValue} */}
+        {/*  stackedArea */}
+        {/*  ChartProps={{ */}
+        {/*    getDisplayValue: getStatValue, */}
+        {/*    tooltip: true, */}
+        {/*  }} */}
+        {/* /> */}
+        {/* <BalancesOverTime */}
+        {/*  label="Silo Deposits" */}
+        {/*  account={account} */}
+        {/*  current={useMemo(() => ([ */}
+        {/*    breakdown.states.deposited.value */}
+        {/*  ]), [breakdown.states.deposited.value])} */}
+        {/*  series={useMemo(() => ([ */}
+        {/*    data.deposits */}
+        {/*  ]), [data.deposits]) as DataPoint[][]} */}
+        {/*  season={season} */}
+        {/*  stats={depositStats} */}
+        {/*  loading={loading} */}
+        {/*  empty={breakdown.states.deposited.value.eq(0)} */}
+        {/* /> */}
       </ModuleContent>
     </Module>
   );
