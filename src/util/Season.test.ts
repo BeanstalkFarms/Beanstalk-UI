dfa953fb98011661012880e2baef9e423ee5bfdf
BeanSama <beansama@bean.farm>
2022-10-17 23:56:46 -0700

    fix: fix deploy errors for Season.test.ts

--- src/util/Season.test.ts
+++ src/util/Season.test.ts
@@ -7,17 +7,17 @@ describe('data prep', () => {
       date: new Date('2022-08-08T00:00:00.000'),
       season: 6074,
       value: 1,
-    };
+    } as unknown as SeasonDataPoint;
     const p2 = {
       date: new Date('2022-08-08T00:01:00.000'),
       season: 6074,
       value: 2,
-    };
+    } as unknown as SeasonDataPoint;
     const p3 = {
       date: new Date('2022-08-08T00:01:00.000'),
       season: 6075,
       value: 2,
-    };
+    } as unknown as SeasonDataPoint;
     const d1 : SeasonDataPoint[] = [p1, p2, p3];
     const d2 : SeasonDataPoint[] = [p2, p1, p3];
     expect(d1.sort(sortSeasons)).toStrictEqual([p1, p2, p3]);
