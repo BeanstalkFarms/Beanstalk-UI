9aa685dea6115b9691a40582fe999ca384e070ca
BeanSama <beansama@bean.farm>
2022-10-25 21:42:45 -0600

    add tooltip to silo balances

--- src/components/Balances/SiloBalances.tsx
+++ src/components/Balances/SiloBalances.tsx
@@ -1,28 +1,47 @@
 import React, { useMemo } from 'react';
-import { Box, Button, Grid, Stack, Typography } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Box, Button, Grid, Stack, Tooltip, Typography, Link } from '@mui/material';
+import {  Link as RouterLink } from 'react-router-dom';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import { useSelector } from 'react-redux';
-import { SEEDS, STALK } from '~/constants/tokens';
+import { SEEDS, STALK, UNRIPE_BEAN, UNRIPE_BEAN_CRV3 } from '~/constants/tokens';
 import useWhitelist from '~/hooks/beanstalk/useWhitelist';
 import { BeanstalkPalette, IconSize } from '../App/muiTheme';
 import Fiat from '~/components/Common/Fiat';
 
 import Row from '../Common/Row';
-import { displayFullBN } from '~/util';
+import { displayFullBN, displayTokenAmount } from '~/util';
 import TokenIcon from '../Common/TokenIcon';
 import { AppState } from '~/state';
-import { ZERO_BN } from '~/constants';
+import { ONE_BN, ZERO_BN } from '~/constants';
 import useFarmerStalkByToken from '~/hooks/farmer/useFarmerStalkByToken';
+import useGetChainToken from '~/hooks/chain/useGetChainToken';
+import useUnripeUnderlyingMap from '~/hooks/beanstalk/useUnripeUnderlying';
+import Stat from '../Common/Stat';
 
 const ARROW_CONTAINER_WIDTH = 20;
+
+const TOOLTIP_COMPONENT_PROPS = {
+  tooltip: {
+    sx: {
+      maxWidth: 'none !important',
+      zIndex: 99999
+      // boxShadow: '0px 6px 20px 10px rgba(255,255,255,0.3) !important'
+    }
+  }
+};
 
 const SiloBalances: React.FC<{}> = () => {
   // Chain Constants
   const whitelist = useWhitelist();
+  const getChainToken = useGetChainToken();
+
+  const urBean        = getChainToken(UNRIPE_BEAN);
+  const urBeanCrv3    = getChainToken(UNRIPE_BEAN_CRV3);
+  const unripeUnderlyingTokens = useUnripeUnderlyingMap();
 
   // State
   const balances = useSelector<AppState, AppState['_farmer']['silo']['balances']>((state) => state._farmer.silo.balances);
+  const unripeTokens  = useSelector<AppState, AppState['_bean']['unripe']>((state) => state._bean.unripe);
   const stalkByToken = useFarmerStalkByToken();
 
   const tokens = useMemo(() => Object.entries(whitelist), [whitelist]);
@@ -91,6 +110,8 @@ const SiloBalances: React.FC<{}> = () => {
       <Stack px={1} py={1} spacing={1}>
         {tokens.map(([address, token]) => {
           const deposits = balances[address]?.deposited;
+          const isUnripe = token === urBean || token === urBeanCrv3;
+          
           return (
             <Box key={`${token.address}-${token.chainId}`}>
               <Button
@@ -108,7 +129,6 @@ const SiloBalances: React.FC<{}> = () => {
                   ':hover': {
                     borderColor: BeanstalkPalette.theme.fallDark.primary
                   }
-                
                 }}
               >
                 <Grid container alignItems="center">
@@ -137,7 +157,7 @@ const SiloBalances: React.FC<{}> = () => {
                     textAlign="left"
                   >
                     <Typography color="text.primary">
-                      {displayFullBN(deposits?.amount ?? ZERO_BN, 0)}{' '}
+                      {displayFullBN(deposits?.amount || ZERO_BN, 0)}{' '}
                       {token.symbol}
                     </Typography>
                   </Grid>
@@ -150,23 +170,84 @@ const SiloBalances: React.FC<{}> = () => {
                     textAlign="right"
                     pr={{ xs: 2, md: 0 }}
                   >
-                    <Row justifyContent="flex-end">
-                      <Typography color="text.primary">
-                        <Fiat
-                          token={token}
-                          amount={deposits?.amount ?? ZERO_BN}
-                        />
-                      </Typography>
-                      <Stack
-                        display={{ xs: 'block', md: 'none' }}
-                        sx={{ width: ARROW_CONTAINER_WIDTH }}
-                        alignItems="center"
-                      >
-                        <ArrowRightIcon
-                          sx={{ color: BeanstalkPalette.lightestGrey }}
-                        />
-                      </Stack>
-                    </Row>
+                    <Box>
+                      <Row justifyContent="flex-end" width="100%">
+                        <Tooltip
+                          placement="left"
+                          componentsProps={TOOLTIP_COMPONENT_PROPS}
+                          title={isUnripe ? (
+                            <Box width="100%">
+                              <Stack direction={{ xs: 'column', md: 'row' }} gap={{ xs: 0, md: 1 }} alignItems="stretch">
+                                <Box px={1} py={0.5}>
+                                  <Stat 
+                                    title={<Row gap={0.5}><TokenIcon token={token} /> {token.symbol}</Row>}
+                                    gap={0.25}
+                                    variant="h4"
+                                    amount={displayTokenAmount(deposits?.amount || ZERO_BN, token, { showName: false })}
+                                    subtitle={
+                                      <> 
+                                        The number of {token.symbol}<br />you have Deposited in the Silo.
+                                      </>
+                                    }
+                                  />
+                                </Box>
+                                <Row>x</Row>
+                                <Box sx={{ px: 1, py: 0.5, maxWidth: 215 }}>
+                                  <Stat
+                                    title="Chop Rate"
+                                    gap={0.25}
+                                    variant="h4"
+                                    amount={`1 - ${(unripeTokens[token.address]?.chopPenalty || ZERO_BN).toFixed(4)}%`}
+                                    subtitle={
+                                      <>
+                                        The current penalty for chopping<br />{token.symbol} for {unripeUnderlyingTokens[token.address].symbol}. <Link href="https://docs.bean.money/farm/barn#chopping" target="_blank" rel="noreferrer" underline="hover" onClick={(e) => { e.stopPropagation(); }}>Learn more</Link>
+                                      </>
+                                    }
+                                />
+                                </Box>
+                                <Row>×</Row>
+                                <Box sx={{ px: 1, py: 0.5, maxWidth: 215 }}>
+                                  <Stat
+                                    title={`${unripeUnderlyingTokens[token.address]} Price`}
+                                    gap={0.25}
+                                    variant="h4"
+                                    amount={(
+                                      <Fiat
+                                        token={unripeUnderlyingTokens[token.address]}
+                                        amount={ONE_BN}
+                                        chop={false}
+                                      />
+                                    )}
+                                    subtitle={`The current price of ${unripeUnderlyingTokens[token.address].symbol}.`}
+                                  />
+                                </Box>
+                                <Stack display={{ xs: 'none', md: 'flex' }} alignItems="center" justifyContent="center">=</Stack>
+                              </Stack>
+                            </Box>) 
+                            : ''
+                          }
+                        >
+                          <Typography color="text.primary" component="span">
+                            <Fiat
+                              token={token}
+                              amount={deposits?.amount ?? ZERO_BN}
+                              />
+                            {isUnripe ? (
+                              <Typography display="inline" color={BeanstalkPalette.washedRed}>*</Typography>
+                              ) : null}
+                          </Typography>
+                        </Tooltip>
+                        <Stack
+                          display={{ xs: 'block', md: 'none' }}
+                          sx={{ width: ARROW_CONTAINER_WIDTH }}
+                          alignItems="center"
+                          >
+                          <ArrowRightIcon
+                            sx={{ color: BeanstalkPalette.lightestGrey }}
+                            />
+                        </Stack>
+                      </Row>
+                    </Box>
                   </Grid>
                   {/** 
                      * Cell: Stalk 
