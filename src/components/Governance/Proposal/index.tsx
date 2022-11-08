f9aa199242a8e44c811ddaa2649d6fb45114a3ec
Silo Chad <silochad@protonmail.com>
2022-11-08 12:05:06 -0600

    update gov proposal markdown layout

--- src/components/Governance/Proposal/index.tsx
+++ src/components/Governance/Proposal/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Card, Divider, Stack, Typography } from '@mui/material';
+import { Box, Card, Stack, Typography } from '@mui/material';
 import MarkdownWrapper from '~/components/Common/MarkdownWrapper';
 import ProposalStats from '~/components/Governance/Proposals/ProposalStats';
 import { Proposal } from '~/util/Governance';
@@ -12,13 +12,13 @@ const ProposalContent: FC<{proposal: Proposal}> = (props) => (
       {/* Title & stats */}
       <Typography variant="h2">{props.proposal?.title}</Typography>
       <ProposalStats proposal={props.proposal} showLink />
-      <Divider sx={{ mt: 1 }}  />
+      {/* <Divider sx={{ mt: 1 }}  /> */}
       {/* Markdown */}
       <Box maxWidth="100%">
         <MarkdownWrapper>{props.proposal?.body}</MarkdownWrapper>
       </Box>
     </Stack>
   </Card>
-  );
+);
 
 export default ProposalContent;
