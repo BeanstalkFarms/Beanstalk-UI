421a719f4866a59ee2770d1c3dde3caf515a1e09
Cool Bean <anoncoolbean@gmail.com>
2022-11-30 10:21:56 -0600

    wip: setup v1 buy order & sell listing forms; soon component for fill actions

--- src/components/Market/PodsV2/forms/BuyPods.tsx
+++ src/components/Market/PodsV2/forms/BuyPods.tsx
@@ -1,31 +1,35 @@
-import { Divider, Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { useAtomValue } from 'jotai';
 import React from 'react';
 import SubActionSelect from '../common/SubActionSelect';
-import SubmitMarketAction from '../common/SubmitMarketAction';
 import { PodOrderType, podsOrderTypeAtom } from '../info/atom-context';
-import FillSellOrder from './FillSellOrder';
 import CreateOrderV2 from '~/components/Market/PodsV2/Actions/CreateOrderV2';
+import Soon from '~/components/Common/ZeroState/Soon';
 
 const BuyPods: React.FC<{}> = () => {
   const orderType = useAtomValue(podsOrderTypeAtom);
 
   return (
     <Stack>
-      <Stack sx={{ p: 0.8 }}>
+      <Stack sx={{ p: 0.8 }} gap={1}>
         {/* buy or sell toggle */}
         <SubActionSelect />
         {/* create buy order */}
         {/* {orderType === PodOrderType.ORDER && <CreateBuyOrder />} */}
         {orderType === PodOrderType.ORDER && <CreateOrderV2 />}
         {/* fill sell order */}
-        {orderType === PodOrderType.FILL && <FillSellOrder />}
+        {/* {orderType === PodOrderType.FILL && <FillSellOrder />} */}
+        {orderType === PodOrderType.FILL && (
+          <Soon>
+            <Typography textAlign="center" color="gray">Select a pod listing on the chart to buy from.</Typography>
+          </Soon>
+        )}
       </Stack>
-      <Divider />
+      {/* <Divider /> */}
       {/* submit buy order */}
-      <Stack p={0.8}>
-        <SubmitMarketAction />
-      </Stack>
+      {/* <Stack p={0.8}> */}
+      {/*  <SubmitMarketAction /> */}
+      {/* </Stack> */}
     </Stack>
   );
 };
--- src/components/Market/PodsV2/forms/SellPods.tsx
+++ src/components/Market/PodsV2/forms/SellPods.tsx
@@ -1,32 +1,36 @@
-import { Stack,  Divider } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import {  useAtomValue } from 'jotai';
 import React from 'react';
 import SubActionSelect from '../common/SubActionSelect';
 import {
  PodOrderType, podsOrderTypeAtom } from '../info/atom-context';
-import SubmitMarketAction from '../common/SubmitMarketAction';
 import CreateListingV2 from '~/components/Market/PodsV2/Actions/CreateListingV2';
-import FillBuyListing from '~/components/Market/PodsV2/forms/FillBuyListing';
+import Soon from '~/components/Common/ZeroState/Soon';
 
 const SellPods: React.FC<{}> = () => {
   const orderType = useAtomValue(podsOrderTypeAtom);
 
   return (
     <Stack>
-      <Stack sx={{ p: 0.8 }}>
+      <Stack sx={{ p: 0.8 }} gap={1}>
         {/* buy or sell toggle */}
         <SubActionSelect />
         {/* create buy order */}
         {/* {orderType === PodOrderType.ORDER && <CreateBuyOrder />} */}
         {orderType === PodOrderType.LIST && <CreateListingV2 />}
         {/* fill sell order */}
-        {orderType === PodOrderType.FILL && <FillBuyListing />}
+        {/* {orderType === PodOrderType.FILL && <FillBuyListing />} */}
+        {orderType === PodOrderType.FILL && (
+          <Soon>
+            <Typography textAlign="center" color="gray">Select a pod order on the chart to sell to.</Typography>
+          </Soon>
+        )}
       </Stack>
-      <Divider />
+      {/* <Divider /> */}
       {/* submit buy order */}
-      <Stack p={0.8}>
-        <SubmitMarketAction />
-      </Stack>
+      {/* <Stack p={0.8}> */}
+      {/*  <SubmitMarketAction /> */}
+      {/* </Stack> */}
     </Stack>
   );
 };
