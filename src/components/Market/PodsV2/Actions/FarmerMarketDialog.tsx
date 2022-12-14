7454cc0f07cb0d1d05133ddb37f2ea4e07df864c
BeanSama <beansama@bean.farm>
2022-12-14 11:59:08 -0800

    wip: market-v1.5: Farmer Market Dialog

--- src/components/Market/PodsV2/Actions/FarmerMarketDialog.tsx
+++ src/components/Market/PodsV2/Actions/FarmerMarketDialog.tsx
@@ -1,16 +1,21 @@
-import { Button, Dialog, IconButton, Stack } from '@mui/material';
+import { Button, Dialog, IconButton, Stack, Typography } from '@mui/material';
 import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
-import { FontSize } from '~/components/App/muiTheme';
+import { BeanstalkPalette, FontSize } from '~/components/App/muiTheme';
 import CondensedCard from '~/components/Common/Card/CondensedCard';
 import { FarmerMarketItem } from '~/hooks/farmer/useFarmerMarket';
 import InfoRow from '~/components/Common/Form/InfoRow';
+import { displayBN, displayFullBN } from '~/util';
+import dynamicPriceIcon from '~/img/misc/curve-adjustment-icon.svg';
+import useHarvestableIndex from '~/hooks/beanstalk/useHarvestableIndex';
 
 type Props = {
   item: FarmerMarketItem | undefined | null;
   open: boolean;
   onClose: () => void;
 };
+
+const openStates = ['ACTIVE', 'CANCELLED_PARTIAL', 'FILLED_PARTIAL'];
 
 const sharedProps = {
   infoVariant: 'bodySmall' as any,
@@ -20,11 +25,11 @@ const sharedProps = {
 };
 
 const FarmerMarketDialog: React.FC<Props> = ({ item, open, onClose }) => {
-  if (
-    !item ||
-    (item?.type === 'order' && !item?.order) ||
-    (item?.type === 'listing' && !item?.listing)
-  ) {
+  const harvestableIndex = useHarvestableIndex();
+  const isOrder = (item?.type === 'order' && item.order !== undefined);
+  const isListing = (item?.type === 'listing' && item.listing !== undefined);
+
+  if (!item || (!isOrder && !isListing)) {
     return null;
   }
 
@@ -37,10 +42,7 @@ const FarmerMarketDialog: React.FC<Props> = ({ item, open, onClose }) => {
             aria-label="close"
             onClick={onClose}
             disableRipple
-            sx={{
-              // color: (theme) => theme.palette.grey[900],
-              p: 0,
-            }}
+            sx={{ p: 0 }}
           >
             <CloseIcon
               sx={{ fontSize: FontSize.base, color: 'text.primary' }}
@@ -51,12 +53,73 @@ const FarmerMarketDialog: React.FC<Props> = ({ item, open, onClose }) => {
         <Stack sx={{ borderTop: '0.5px solid', borderColor: 'divider' }}>
           <Stack
             sx={{ borderBottom: '0.5px solid', borderColor: 'divider' }}
-            gap={1}
+            gap={2}
             px={1.5}
             py={1}
           >
             <InfoRow label="ACTION" {...sharedProps}>
               {item.action.toUpperCase()}
+            </InfoRow>
+            <InfoRow label="TYPE" {...sharedProps}>
+              {item.type.toUpperCase()}
+            </InfoRow>
+            <InfoRow label="PRICE TYPE" {...sharedProps}>
+              {item.priceType.toUpperCase()}
+            </InfoRow>
+            <InfoRow label="PRICE" {...sharedProps}>
+              {item.priceType === 'dynamic' ? (
+                <Typography
+                  variant="bodySmall"
+                  color="text.secondary"
+                  component="span"
+                >
+                  {displayFullBN(item.pricePerPod, 2)}
+                  <img
+                    alt=""
+                    src={dynamicPriceIcon}
+                    style={{ height: 'inherit', width: 'auto' }}
+                  />
+                </Typography>
+              ) : (
+                displayFullBN(item.pricePerPod, 2)
+              )}
+            </InfoRow>
+            {isListing && (
+              <InfoRow label="AMOUNT" {...sharedProps}>
+                {`${displayBN(item.remainingAmount)} PODS`}
+              </InfoRow>
+            )}
+            <InfoRow label="PLACE IN LINE" {...sharedProps}>
+              {`${isListing ? '0 - ' : ''}${displayBN(
+                item.placeInPodline,
+              )} PODS`}
+            </InfoRow>
+            {isListing && (
+              <InfoRow label="EXPIRY">
+                {`${
+                  item.listing?.maxHarvestableIndex?.gt(0)
+                    ? displayBN(item.listing?.maxHarvestableIndex.minus(harvestableIndex))
+                    : 'N/A'
+                }`}
+              </InfoRow>
+            )}
+            <InfoRow label="% Filled" {...sharedProps}>
+              {`${displayFullBN(item.fillPct, 2)}%`}
+            </InfoRow>
+            <InfoRow label="TOTAL" {...sharedProps}>
+              {`${displayFullBN(item.remainingAmount.div(item.pricePerPod), 2)} BEAN`}
+            </InfoRow>
+            <InfoRow label="STATUS" {...sharedProps}>
+              <Typography
+                variant="bodySmall"
+                sx={{
+                  color: openStates.includes(item.status)
+                    ? BeanstalkPalette.theme.winter.orderGreen
+                    : 'text.primary',
+                }}
+              >
+                {item.status.toUpperCase()}
+              </Typography>
             </InfoRow>
           </Stack>
           <Stack
@@ -66,7 +129,7 @@ const FarmerMarketDialog: React.FC<Props> = ({ item, open, onClose }) => {
             gap={1}
             p={1}
           >
-            <Button variant="text" sx={{ color: 'red', padding: 1 }}>
+            <Button variant="text" sx={{ color: 'red', padding: 1 }} onClick={onClose}>
               CANCEL
             </Button>
             <Button
